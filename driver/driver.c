f243867f7fd19435dbce7a03e033d955e2024c50
Andrea Marini <andrea.marini@cnr.it>
2007-10-02 11:28:53 +0000

    V 3.0.11_01-10-07
    *  config/Makefile.in 2io/ioBSS_Haydock.F 7bse/K_Haydock.F 7bse/Kernel_by_V.F driver/driver.c sbin/mkmkfl.in 
    
    Additions:
    - Added PJ_NO_HERM_LIBS project (self_no_herm) for Myrta in K_Haydock.F
    - Small modifications in Kernel_by_V.F. Now it loads K also when
      it=0
    
    Changes:
    - Removed Vnp1 entry from ioBSS_Haydock (was not used)
    
    Patch sent by: Andrea
    
    
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk@160 76a4dfbf-5f1f-0410-a543-a0282073b526

--- driver/driver.c
+++ driver/driver.c
@@ -46,7 +46,7 @@ typedef struct
  #define F90_FUNC_(name,NAME) name
 #endif
 #include "codever.h"
-#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC
+#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC || PJ_NO_HERM
  #include "self_cpp.h"
 #endif
 #if defined spp  || SPP_PJ_PH || SPP_PJ_RAS
@@ -122,7 +122,7 @@ main(int argc, char *argv[])
 /* 
  Switch off MPI_init as I have options used to create the input file...
 */
-#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC
+#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC || PJ_NO_HERM
      if (j> 9) {mpi_init=-1;};
 #endif
 #if defined spp || SPP_PJ_PH || SPP_PJ_RAS
@@ -203,7 +203,7 @@ main(int argc, char *argv[])
    MPI_Comm_size(MPI_COMM_WORLD, &np);  /* get number of processes */
  };
 #endif
-#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC
+#if defined self  || PJ_RAS || PJ_REELS || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC || PJ_NO_HERM
  /* 
    Running the Fortran SELF driver 
  ===========================================================================
@@ -257,7 +257,7 @@ main(int argc, char *argv[])
  */
  strcpy(edit_line,"vi ");
  strncat(edit_line,inf,strlen(inf));
-#if defined self  || PJ_RAS || PJ_REELS || spp || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC || SPP_PJ_PH || SPP_PJ_RAS
+#if defined self  || PJ_RAS || PJ_REELS || spp || PJ_PH || PJ_GKS || PJ_CYL_CUT || PJ_SPIN || PJ_SC || SPP_PJ_PH || SPP_PJ_RAS || PJ_NO_HERM
  if (iif == 1 && ttd>0 ) 
  {
   system(edit_line);

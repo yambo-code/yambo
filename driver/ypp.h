7bacdae35ac66dd6d49c97785114f110099d6dc9
Andrea Marini <andrea.marini@cnr.it>
2008-08-12 11:56:03 +0000

    GPL ypp/yambo options added. Gfortran bug-fix
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk@288 76a4dfbf-5f1f-0410-a543-a0282073b526

--- driver/ypp.h
+++ driver/ypp.h
@@ -20,20 +20,21 @@
   MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
 */
  static Ldes opts[] = { /* Int Real Ch (Dummy)*/
-  {"ifile",  "F","Input file",0,0,1,0},              
-  {"help","h","Short Help",0,0,0,0}, 
-  {"lhelp","H","Long Help",0,0,0,0}, 
+  {"ifile", "F","Input file",0,0,1,0},              
+  {"help",  "h","Short Help",0,0,0,0}, 
+  {"lhelp", "H","Long Help",0,0,0,0}, 
   {"jobstr","J","Job string identifier",0,0,1,0},   
-  {"ifile",  "F","Input file",0,0,1,0},              
-  {"idir",   "I","Core I/O directory",0,0,1,0},         
-  {"odir",   "O","Additional I/O directory",0,0,1,0},        
+  {"ifile", "F","Input file",0,0,1,0},              
+  {"idir",  "I","Core I/O directory",0,0,1,0},         
+  {"odir",  "O","Additional I/O directory",0,0,1,0},        
+  {"cdir",   "C","Communications I/O directory",0,0,1,0},
   {"nompi", "N","Skip MPI initialization",0,0,0,0}, 
   {"dbfrag","S","DataBases fragmentation",0,0,0,0}, 
-  {"kpt","k","K-grid generator",0,0,0,0}, 
-  {"excp","e","Excitons [(s)ort,(sp)in,(a)mplitude]",0,0,1,0}, 
-  {"plot","p","Plot [(e)xciton,(m)agnetization,(em),(d)ensity,(w)ave]",0,0,1,0}, 
+  {"kpt",   "k","K-grid generator",0,0,0,0}, 
+  {"excp",  "e","Excitons [(s)ort,(sp)in,(a)mplitude]",0,0,1,0}, 
+  {"plot",  "p","Plot [(e)xciton,(m)agnetization,(em),(d)ensity,(w)ave]",0,0,1,0}, 
   {"freehole","f","Free hole position [excitonic plot]",0,0,0,0}, 
-  {"bzrim","r","BZ energy RIM analyzer",0,0,0,0}, 
+  {"bzrim",   "r","BZ energy RIM analyzer",0,0,0,0}, 
 #if defined _YPP_ELPH
   {"elph", "p","Electron-Phonon DBs preprocessing",0,0,0,0}, 
 #endif

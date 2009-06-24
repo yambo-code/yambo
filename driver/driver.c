5bb3c16dcf7dc68da5f8e2833447a1abbe0908a1
Andrea Marini <andrea.marini@cnr.it>
2009-06-24 11:59:07 +0000

    _DOUBLES Project removed (Andrea & Davide)
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk@438 76a4dfbf-5f1f-0410-a543-a0282073b526

--- driver/driver.c
+++ driver/driver.c
@@ -55,7 +55,7 @@ typedef struct
 #if defined _MANYK || _TB || _MAGNON || _DIPOLE_RIM || _PARTIAL_DIAGO
  #define _YAMBO_MAIN
 #endif
-#if defined _DOUBLES || _SOLITONS || _TEST_1
+#if defined _SOLITONS || _TEST_1
  #define _YAMBO_MAIN
 #endif
 #if defined _ypp  || _YPP_ELPH || _YPP_RAS || _YPP_RT

5055f8a317bea41de5fdb7820b0d35f8d1fba41f
Andrea Marini <andrea.marini@cnr.it>
2011-12-28 11:21:58 +0000

    Version 3.2.4
    *  configure include/version.inc config/configure.ac pol_function/Dipole_check_shifted_grids.F pol_function/Dipole_shifted_grids.F interface/init_load.F interface/init.F real_time/RT_ELPH_Kernel.F modules/mod_X_output.F ypp/ypp_init.F ypp/mod_YPP.F ypp/ypp_init_load.F ypp/k_grids.F driver/ypp.h 
    
    Changes:
    - Partial Fix of optical calculations using shifted grids. Eps0 calculated with the shifted grids agree with the calculation with 
      the non-local part of the pseudo. There is still  a large prefactor (~70) I could not find the reason of. Also the
      generation of the grids by ypp has been changed.
      *** this piece of code is still to test and improve ***
    
    Patch sent by: Andrea
    
    
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk@1002 76a4dfbf-5f1f-0410-a543-a0282073b526

--- driver/ypp.h
+++ driver/ypp.h
@@ -47,7 +47,7 @@
   {"cdir",  "C","Communications I/O directory",0,0,1,0,1},
   {"nompi", "N","Skip MPI initialization",0,0,0,0,0}, 
   {"dbfrag","S","DataBases fragmentation",0,0,0,0,1}, 
-  {"bzgrids","k","BZ Grid generator [(k)pt,(q)pt,(l)ongitudinal,(h)igh symmetry]",0,0,1,0,0}, 
+  {"bzgrids","k","BZ Grid generator [(k)pt,(q)pt,(s)hifted,(h)igh symmetry]",0,0,1,0,0}, 
   {"qpdb",   "q","Generate/modify a quasi-particle database",0,0,0,0,0}, 
 #if defined _YPP_SC 
   {"MeanPot",  "v","Mean Potential",0,0,0,0,0}, 

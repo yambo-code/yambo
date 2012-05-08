cd7cd538d327b554323c3a57a60e1770359baa22
Andrea Marini <andrea.marini@cnr.it>
2012-05-08 07:18:07 +0000

    Merged with trunk @ revision 1197
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/branches/devel-andrea@1202 76a4dfbf-5f1f-0410-a543-a0282073b526

--- sbin/yamboo.pl
+++ sbin/yamboo.pl
@@ -26,7 +26,7 @@ $manual_preprocess_files = " ";
 $exclude_files = "yamboo.pl ";
 @core_projects = ('yambo','ypp','p2y','a2y','f2y','e2y');
 @user_projects = ('PW_DIAGO','MAGNETIC','DISTRIBUTED','SC','RT','MANYK','ELPH','POLARON',
-                  'RAS','REELS','YPP_RAS','TEST_1','YPP_ELPH','YPP_RAS','YPP_RT',
+                  'TEST_1','YPP_ELPH','YPP_RT',
                   'YPP_SC','YPP_MAGNETIC','BOLTZMANN','YPP_BOLTZMANN','DEBUG','AUGER',
                   'TEST_2');
 $user_projects_string = join(" ",@user_projects); # Append a space

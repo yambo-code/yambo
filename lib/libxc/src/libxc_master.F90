266c7df9ce9ff78507afd83fa04df8afc68d4a0a
Pedro Melo <pmelo1089@gmail.com>
2014-06-27 12:36:49 +0000

    Integrals and evaluation of the electron-photon self-energy added.
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/branches/devel-pedro@3458 76a4dfbf-5f1f-0410-a543-a0282073b526

--- lib/libxc/src/libxc_master.F90
+++ lib/libxc/src/libxc_master.F90
@@ -1,22 +1,3 @@
-!! Copyright (C) 2003-2006 M. Marques, A. Castro, A. Rubio, G. Bertsch
-!!
-!! This program is free software; you can redistribute it and/or modify
-!! it under the terms of the GNU Lesser General Public License as published by
-!! the Free Software Foundation; either version 2, or (at your option)
-!! any later version.
-!!
-!! This program is distributed in the hope that it will be useful,
-!! but WITHOUT ANY WARRANTY; without even the implied warranty of
-!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-!! GNU Lesser General Public License for more details.
-!!
-!! You should have received a copy of the GNU Lesser General Public License
-!! along with this program; if not, write to the Free Software
-!! Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-!! 02111-1307, USA.
-!!
-!! $Id: libxc.f90 3550 2007-11-19 14:32:49Z marques $
-
 #if SINGLE_PRECISION
 #  define XC_F90(x) xc_s_f90_ ## x
 #else
@@ -24,7 +5,6 @@
 #endif
 
 
-!-------------------------------------------------------------------
 module XC_F90(types_m)
 #if SINGLE_PRECISION
   integer, public, parameter :: xc_f90_kind = selected_real_kind(4)
@@ -40,7 +20,6 @@ module XC_F90(types_m)
 end module XC_F90(types_m)
 
 
-!-------------------------------------------------------------------
 module XC_F90(lib_m)
   
   use XC_F90(types_m)
@@ -50,7 +29,6 @@ module XC_F90(lib_m)
 
   public
 
-  ! Families of xc functionals
   integer, parameter ::     &
     XC_FAMILY_UNKNOWN       =  -1,  &
     XC_FAMILY_NONE          =   0,  &

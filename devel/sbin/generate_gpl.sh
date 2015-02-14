7971b9fb6fe745fe810dccffe34cd00dcd1a2c72
Davide Sangalli <davide.sangalli@gmail.com>
2015-02-14 16:25:26 +0000

    Version 4.0.0
    *  config/configure.ac configure include/version.inc sbin/generate_gpl.sh bse/K_Haydock.F bse/K_diagonalization.F bse/K_stored_in_a_BIG_matrix.F io/io_BSS_Haydock.F modules/mod_BS.F modules/mod_KERR.F 
    
    Bugs:
    - 
    Additions:
    - 
    Changes:
    - [yambo_kerr] working on yambo_kerr beyond IP in the trunk devel
    
    Patch sent by: Davide
    
    
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk@4055 76a4dfbf-5f1f-0410-a543-a0282073b526

--- devel/sbin/generate_gpl.sh
+++ devel/sbin/generate_gpl.sh
@@ -3,7 +3,7 @@ TRUNK_DIR=/home/sangalli/Data/Lavoro/Codici/yambo/the_wole_project/trunk/devel
 rm -r $TARGET_DIR/*
 cp -r $TRUNK_DIR/* $TARGET_DIR/
 cd $TARGET_DIR
- ./sbin/yamboo_new.pl -p="KERR SURF YPP_SURF ELPH YPP_ELPH FFTW FFTSG OPENMP"
+ ./sbin/yamboo_new.pl -p="KERR SURF YPP_SURF ELPH YPP_ELPH FFTW FFTSG OPENMP MPI"
  chmod +x delete.batch 
 ./delete.batch
 svn status | grep -v libxc

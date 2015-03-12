9e93642473b240c9504e994093dd543337fc9478
Andrea Marini <andrea.marini@cnr.it>
2015-03-12 15:48:30 +0000

    GIANT merge of devel-andrea in trunk-devel. Tests updated and passed by both. GPL prepared for update
    git-svn-id: svn+ssh://qeforge.qe-forge.org/svnroot/yambo-devel/trunk/devel@4143 76a4dfbf-5f1f-0410-a543-a0282073b526

--- driver/yambo.h
+++ driver/yambo.h
@@ -81,7 +81,7 @@
 #if defined _SC | defined _MAGNETIC | defined _RT
   {"collisions", "e","Evaluate Collisions",0,0,0,0,0}, 
   {"scpot",  "v","Self-Consistent Potential",0,0,1,0,0}, 
-  {"DESC",   " ","opt=(hf),(c)ohsex,(exx),(exxc),(srpa),(h)artree,(d)ef",0,0,0,0,0},
+  {"DESC",   " ","opt=(hf),(c)ohsex,(exx),(exxc),(srpa),(h)artree,(d)ef,(ip)",0,0,0,0,0},
 #endif
 #if defined _MAGNETIC 
   {"magnetic", "m","Magnetic [opt=(p)auli,(l)andau,(a)ll]",0,0,1,0,0}, 

33c8218685350b144cd48c0f24c810e3fe199ce0
Davide Sangalli <davide.sangalli@gmail.com>
2016-04-01 00:44:49 +0200

    revision updated

--- sbin/git_hooks.sh
+++ sbin/git_hooks.sh
@@ -36,5 +36,9 @@ chmod +x .git/hooks/pre-commit
 cat <<EOF > .git/hooks/pre-push
 #!/bin/bash
 sbin/yambo_versions_update.tcsh r
+git add config/configure.ac
+git add configure
+git add include/version.inc
+git commit -m "revision updated" 
 EOF
 chmod +x .git/hooks/pre-push

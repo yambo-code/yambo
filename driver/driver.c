1795a6a4bb569a5b26dd9c1e0a6d95313eefed1e
Andrea Marini <andrea.marini@cnr.it>
2017-02-17 01:24:04 +0100

    Additions:
    - *** BSE/TDDFT beyond the TDA ***
      It is now possible to calculate TDDFT and BSE response functions beyond the
      Tamm-Dancoff approximation. The antiresonant and coupling blocks of the
      Hamiltonians can be computed.
      The final Dyson equation is, then. solved using diagonalization or the
      quasi-Hermitian Haydock algorithm proposed in Nano Letters, 9, 2820 (2009)
    
    - *** LIBXC support ***
      This means that Yambo can ALWAYS read GS (abinit) databases generated using libxc.
      If, in addition, yambo is linked against LIBXC (see --with-libxc-include/--with-libxc-lib options
      of configure) this is used to evalute DFT xc terms.
    
    - *** REAL AXIS GW ***
      It is now possible to calculate the GW self-energy on the real-axis. This means no plasmon-pole
      approx and the possibility of calculating electronic lifetimes.
      In addition the secant (recursive) solver of the Dyson equation has been added.
    
    - *** CUBE format ***
      Added support plot 3D isosurfaces in Gaussian cube format.

--- driver/driver.c
+++ driver/driver.c
@@ -67,6 +67,9 @@ typedef struct
 #if defined _a2y
  #include "a2y.h"
 #endif
+#if defined _c2y
+ #include "c2y.h"
+#endif
 #if defined _f2y
  #include "f2y.h"
 #endif
@@ -266,6 +269,14 @@ main(int argc, char *argv[])
  ===========================================================================
  */
  F90_FUNC(ypp_i,YPP_I)(
+          rnstr2,&lni,inf,&iif,id,&iid,od,&iod,com_dir,&icd,js,&ijs,&np,&pid);
+#endif
+#if defined _c2y 
+ /* 
+   Running the Fortran c2y driver
+ ===========================================================================
+ */
+ F90_FUNC(c2y_i,C2Y_I)(
           rnstr2,&lni,inf,&iif,id,&iid,od,&iod,com_dir,&icd,js,&ijs,&np,&pid);
 #endif
 #if defined _a2y 

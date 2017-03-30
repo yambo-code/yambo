0b447b1860ffc32678bf0e0e06b5cba477240596
Davide Sangalli <davide.sangalli@gmail.com>
2017-03-31 01:40:26 +0200

    Version 4.2.0,  Revision 14440,  Hash bb5988e
    MODIFIED *  config/version.m4 configure driver/yambo.h driver/yambo_driver.F include/version.inc interface/INIT_load.F
    
    Changes:
    - [yambo] Dipoles runlevel is now working
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- driver/yambo.h
+++ driver/yambo.h
@@ -69,7 +69,7 @@
   {"HF_and_locXC",  "x","Hartree-Fock Self-energy and local XC",0,0,0,0,0},      
   {"em1d",   "d","Dynamical Inverse Dielectric Matrix",0,0,0,0,0},     
   {"em1s",   "b","Static Inverse Dielectric Matrix",0,0,0,0,0},        
-  {"dipoles","o","Compute oscillator strenghts (or dipoles)",0,0,0,0,0},        
+  {"dipoles","q","Compute oscillator strenghts (or dipoles)",0,0,0,0,0},        
   {"gwapprx","p","GW approximations [opt=(p)PA/(c)HOSEX]",0,0,1,0,0},              
   {"gw0",    "g","Dyson Equation solver",0,0,1,0,0}, 
   {"DESC",   " ","[opt=(n)ewton/(s)ecant/(g)reen]",0,0,0,0,0},
@@ -79,7 +79,7 @@
   {"photolum", "u","Photo-Luminescence",0,0,0,0,0}, 
 #endif
 #if defined _RT
-  {"negf",   "q","Real-time dynamic [opt=(p)ump or probe,(pp)ump & probe, (pn) n external fields]",0,0,1,0,0}, 
+  {"negf",   "n","Real-time dynamic [opt=(p)ump or probe,(pp)ump & probe, (pn) n external fields]",0,0,1,0,0}, 
   {"scattp", "s","Scattering  [opt=(e)lectrons/(p)honons/p(h)otons/(a)ll]",0,0,1,0,0},
 #endif
 #if defined _QED && !defined _ELPH

yambo-code/yambo

commit 7faad77b5597e861e149190e54778c4601d5bc3c

Version 4.3.0,  Revision 15304,  Hash 3566bba
MODIFIED *  config/version.m4 config/version.m4_gpl configure include/version.inc bse/K_slepc_driver.F

Bugs:
- [yambo] Small fix which was preventing yambo from compiling with external slepc libs

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 4.3.0 r.15303 h.be6c5c4, yambo@yambo-code.org)
+AC_INIT(Yambo, 4.3.0 r.15304 h.3566bba, yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
-SREVISION="15303"
-SHASH="be6c5c4"
+SREVISION="15304"
+SHASH="3566bba"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.3.0 r.108 (based on r.15303 h.be6c5c4), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.3.0 r.108 (based on r.15304 h.3566bba), yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="15303"
-SHASH="be6c5c4"
+BASE_REV="15304"
+SHASH="3566bba"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 4.3.0 r.15303 h.be6c5c4.
+# Generated by GNU Autoconf 2.69 for Yambo 4.3.0 r.15304 h.3566bba.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='4.3.0 r.15303 h.be6c5c4'
-PACKAGE_STRING='Yambo 4.3.0 r.15303 h.be6c5c4'
+PACKAGE_VERSION='4.3.0 r.15304 h.3566bba'
+PACKAGE_STRING='Yambo 4.3.0 r.15304 h.3566bba'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1490,7 +1490,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 4.3.0 r.15303 h.be6c5c4 to adapt to many kinds of systems.
+\`configure' configures Yambo 4.3.0 r.15304 h.3566bba to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1556,7 +1556,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 4.3.0 r.15303 h.be6c5c4:";;
+     short | recursive ) echo "Configuration of Yambo 4.3.0 r.15304 h.3566bba:";;
    esac
   cat <<\_ACEOF
 
@@ -1744,7 +1744,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 4.3.0 r.15303 h.be6c5c4
+Yambo configure 4.3.0 r.15304 h.3566bba
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2423,7 +2423,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 4.3.0 r.15303 h.be6c5c4, which was
+It was created by Yambo $as_me 4.3.0 r.15304 h.3566bba, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2774,8 +2774,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
-SREVISION="15303"
-SHASH="be6c5c4"
+SREVISION="15304"
+SHASH="3566bba"
 
 
 
@@ -13767,7 +13767,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 4.3.0 r.15303 h.be6c5c4, which was
+This file was extended by Yambo $as_me 4.3.0 r.15304 h.3566bba, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -13829,7 +13829,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 4.3.0 r.15303 h.be6c5c4
+Yambo config.status 4.3.0 r.15304 h.3566bba
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

include/version.inc
@@ -1,6 +1,6 @@
 code_version(1)=4
 code_version(2)=3
 code_version(3)=0
-code_hash='be6c5c4'
-code_revision=15303
+code_hash='3566bba'
+code_revision=15304
 code_GPL_revision=108

src/bse/K_slepc_driver.F
@@ -559,7 +559,7 @@ subroutine K_multiply_by_V_slepc(slepc_mat,vi,vo,ierr)
  PetscScalar, pointer :: xx(:)
  Vec                  :: vi,vo,x
  Mat                  :: slepc_mat
- integer              :: n, i_g, i_c, ierr, H_pos, start_index, low, high
+ integer              :: n, i_g, i_c, ierr, H_pos(1), start_index, low, high
  !
  ! create scattering context vi (distributed) -> x (local)
  !

commit 00d64dad0a7d0c97925189bf558aafddf0280ae9

Version 4.3.0,  Revision 15320,  Hash 8fbb882
MODIFIED *  config/version.m4 config/version.m4_gpl configure include/version.inc bse/K_slepc_driver.F linear_algebra/MATRIX_slepc.F modules/mod_interfaces.F

Bugs:
-[yambo] Fixed problem introduced with commit 7faad77b5597e861e149190e54778c4601d5bc3c
         Still at present slepc solver works only with internal slepc

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 4.3.0 r.15321 h.5bc84cb, yambo@yambo-code.org)
+AC_INIT(Yambo, 4.3.0 r.15323 h.8fbb882, yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
-SREVISION="15321"
-SHASH="5bc84cb"
+SREVISION="15323"
+SHASH="8fbb882"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.3.0 r.108 (based on r.15321 h.5bc84cb), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.3.0 r.108 (based on r.15323 h.8fbb882), yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="15321"
-SHASH="5bc84cb"
+BASE_REV="15323"
+SHASH="8fbb882"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 4.3.0 r.15321 h.5bc84cb.
+# Generated by GNU Autoconf 2.69 for Yambo 4.3.0 r.15323 h.8fbb882.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='4.3.0 r.15321 h.5bc84cb'
-PACKAGE_STRING='Yambo 4.3.0 r.15321 h.5bc84cb'
+PACKAGE_VERSION='4.3.0 r.15323 h.8fbb882'
+PACKAGE_STRING='Yambo 4.3.0 r.15323 h.8fbb882'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1490,7 +1490,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 4.3.0 r.15321 h.5bc84cb to adapt to many kinds of systems.
+\`configure' configures Yambo 4.3.0 r.15323 h.8fbb882 to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1556,7 +1556,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 4.3.0 r.15321 h.5bc84cb:";;
+     short | recursive ) echo "Configuration of Yambo 4.3.0 r.15323 h.8fbb882:";;
    esac
   cat <<\_ACEOF
 
@@ -1744,7 +1744,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 4.3.0 r.15321 h.5bc84cb
+Yambo configure 4.3.0 r.15323 h.8fbb882
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2423,7 +2423,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 4.3.0 r.15321 h.5bc84cb, which was
+It was created by Yambo $as_me 4.3.0 r.15323 h.8fbb882, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2774,8 +2774,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="4"
 SSUBVERSION="3"
 SPATCHLEVEL="0"
-SREVISION="15321"
-SHASH="5bc84cb"
+SREVISION="15323"
+SHASH="8fbb882"
 
 
 
@@ -13767,7 +13767,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 4.3.0 r.15321 h.5bc84cb, which was
+This file was extended by Yambo $as_me 4.3.0 r.15323 h.8fbb882, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -13829,7 +13829,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 4.3.0 r.15321 h.5bc84cb
+Yambo config.status 4.3.0 r.15323 h.8fbb882
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

include/version.inc
@@ -1,6 +1,6 @@
 code_version(1)=4
 code_version(2)=3
 code_version(3)=0
-code_hash='5bc84cb'
-code_revision=15321
+code_hash='8fbb882'
+code_revision=15323
 code_GPL_revision=6459

src/bse/K_slepc_driver.F
@@ -62,11 +62,20 @@ subroutine K_slepc_driver(iq,W)
 #include <slepc/finclude/slepceps.h>
  !
  Mat           slepc_mat ! Slepc matrix where the hamiltonian will be stored
- Vec           vin,vout
  type(w_samp)  :: W
  integer       :: iq
  logical       :: K_is_not_hermitian
- integer       :: ierr
+ !
+ PetscErrorCode        :: ierr
+ PetscFortranInt       :: slepc_ncv
+#if defined _DOUBLE
+ PetscFortranDouble    :: slepc_tol
+ PetscFortranDouble    :: slepc_target_E
+#else
+ PetscFortranFloat     :: slepc_tol
+ PetscFortranFloat     :: slepc_target_E
+#endif
+ !
  !
  ! Residuals & Energies
  !
@@ -83,9 +92,6 @@ subroutine K_slepc_driver(iq,W)
  complex(SP),allocatable :: BS_V_right(:,:)     ! Coupling
  !
  integer              ::io_err,ID  ! I/O
- !
- PetscFortranInt     :: vec_index(4)
- PetscFortranComplex :: vec_value(4)
  !
  call timing('Slepc Solver',OPR='start')
  !  
@@ -120,6 +126,9 @@ subroutine K_slepc_driver(iq,W)
    BSS_n_eig = 1
  endif
  !
+ slepc_ncv      = BSS_slepc_ncv
+ slepc_tol      = BSS_slepc_tol
+ slepc_target_E = BSS_target_E
  !
  ! Eigenstates 2 DB ?
  !====================
@@ -190,15 +199,15 @@ subroutine K_slepc_driver(iq,W)
      !
      call MATRIX_slepc(slepc_mat,BSS_n_eig,E_cmpl=BS_E,&
 &                      V_right=BS_V_right,V_left=BS_V_left,&
-&                      target_energy=BSS_target_E, extraction=BSS_slepc_extraction,&
-&                      slepc_ncv=BSS_slepc_ncv,slepc_tol=BSS_slepc_tol)
+&                      target_energy=slepc_target_E, extraction=BSS_slepc_extraction,&
+&                      slepc_ncv=slepc_ncv,slepc_tol=slepc_tol)
      !
    else
      !
      call MATRIX_slepc(slepc_mat,BSS_n_eig,E_real=BS_E_real,&
 &                      V_right=BS_mat,&
-&                      target_energy=BSS_target_E, extraction=BSS_slepc_extraction,&
-&                      slepc_ncv=BSS_slepc_ncv,slepc_tol=BSS_slepc_tol)
+&                      target_energy=slepc_target_E, extraction=BSS_slepc_extraction,&
+&                      slepc_ncv=slepc_ncv,slepc_tol=slepc_tol)
      !
      BS_E=cmplx(BS_E_real,0._SP,SP)
      !
@@ -357,10 +366,10 @@ subroutine K_stored_in_a_slepc_matrix(iq,slepc_mat)
  !
  !
  use pars,           ONLY:SP,DP,cI,cONE
- use electrons,      ONLY:n_sp_pol,E_SOC_nbands
- use BS,             ONLY:BS_K_dim,BS_H_dim,BS_nT_grps,BS_blk,n_BS_blks,BS_T_grp,BS_K_coupling
- use BS_solvers,     ONLY:BSS_dipoles,BSS_eh_table,BSS_eh_E,BSS_eh_f,BSS_eh_W,BSS_eh_Z,&
-&                         BSS_eh_E_SOC_corr,BSS_perturbative_width,BSS_alloc
+ use electrons,      ONLY:n_sp_pol
+ use BS,             ONLY:BS_K_dim,BS_H_dim,BS_blk,n_BS_blks,BS_T_grp,BS_K_coupling
+ use BS_solvers,     ONLY:BSS_eh_E,BSS_eh_W,&
+&                         BSS_perturbative_width,BSS_alloc
  use com,            ONLY:error,msg
  !
  implicit none
@@ -373,15 +382,19 @@ subroutine K_stored_in_a_slepc_matrix(iq,slepc_mat)
 #include <slepc/finclude/slepceps.h>
  !
  complex(SP) :: E_plus_W
- integer     :: iq,i_c,i_r,H_pos(2),i_Tk,i_Tp,i_B,i_g,i_k_bz,i_v,i_cond,i_spin
- integer     :: ierr
+ integer     :: iq,i_c,i_r,i_Tk,i_Tp,i_B
  Mat   :: slepc_mat
  PetscFortranComplex :: val
+ PetscFortranInt     :: H_pos(2),SL_K_dim,SL_H_dim
+ PetscErrorCode      :: ierr
+ !
+ SL_K_dim=BS_K_dim
+ SL_H_dim=BS_H_dim
  !
  ! Allocate the slepc Matrix
  !
  call MatCreate(PETSC_COMM_WORLD,slepc_mat,ierr)
- call MatSetSizes(slepc_mat,PETSC_DECIDE,PETSC_DECIDE,BS_H_dim,BS_H_dim,ierr)
+ call MatSetSizes(slepc_mat,PETSC_DECIDE,PETSC_DECIDE,SL_H_dim,SL_H_dim,ierr)
  call MatSetType(slepc_mat, MATMPIDENSE,ierr)
  call MatSetUp(slepc_mat,ierr)
  ! 
@@ -390,6 +403,7 @@ subroutine K_stored_in_a_slepc_matrix(iq,slepc_mat)
  call fill_eh_dipoles()
  ! 
  ! filling of the slepc_mat
+ !
  !
  do i_B=1,n_BS_blks
    !
@@ -412,7 +426,7 @@ subroutine K_stored_in_a_slepc_matrix(iq,slepc_mat)
          call MatSetValue( slepc_mat, H_pos(1),          H_pos(1), val, INSERT_VALUES, ierr )
        else
          val = BS_blk(i_B)%mat(i_r,i_r)
-         call MatSetValue( slepc_mat, H_pos(1)+BS_K_dim, H_pos(1), val, INSERT_VALUES, ierr )
+         call MatSetValue( slepc_mat, H_pos(1)+SL_K_dim, H_pos(1), val, INSERT_VALUES, ierr )
        endif
        !
      endif
@@ -432,8 +446,8 @@ subroutine K_stored_in_a_slepc_matrix(iq,slepc_mat)
        endif
        !
        if (BS_blk(i_B)%mode=="A") then
-         call MatSetValue( slepc_mat, H_pos(1), H_pos(2)+BS_K_dim,       val , INSERT_VALUES, ierr )
-         call MatSetValue( slepc_mat, H_pos(2)+BS_K_dim, H_pos(1), conjg(val), INSERT_VALUES, ierr )
+         call MatSetValue( slepc_mat, H_pos(1), H_pos(2)+SL_K_dim,       val , INSERT_VALUES, ierr )
+         call MatSetValue( slepc_mat, H_pos(2)+SL_K_dim, H_pos(1), conjg(val), INSERT_VALUES, ierr )
        endif
        !
      enddo
@@ -477,13 +491,13 @@ subroutine K_shell_matrix(iq,slepc_mat)
 #include <slepc/finclude/slepcsys.h>
 #include <slepc/finclude/slepceps.h>
  !
- !REMOVE
- integer        i_g
- !REMOVE 
  integer        iq, i_B, i_r, i_c, i_Tk, i_Tp
- Mat            slepc_mat
- PetscErrorCode ierr
+ PetscFortranInt  :: SL_H_dim,SL_K_dim
+ Mat              :: slepc_mat
+ PetscErrorCode   :: ierr
  external K_multiply_by_V_slepc
+ !
+ SL_H_dim=BS_H_dim
  !
  ! Allocate haydock vectors
  ! 
@@ -499,7 +513,7 @@ subroutine K_shell_matrix(iq,slepc_mat)
  ! TODO: In the future it should be done acording to the BS parallelization
  ! to avoid the scattering vi (distributed) -> x (local) in K_multiply_by_V_slepc
  !
- call MatCreateShell(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,BS_H_dim,BS_H_dim,0,slepc_mat,ierr)
+ call MatCreateShell(PETSC_COMM_WORLD,PETSC_DECIDE,PETSC_DECIDE,SL_H_dim,SL_H_dim,0,slepc_mat,ierr)
  call MatAssemblyBegin(slepc_mat,MAT_FINAL_ASSEMBLY,ierr)
  call MatAssemblyEnd(slepc_mat,MAT_FINAL_ASSEMBLY,ierr)
  call MatShellSetOperation(slepc_mat,MATOP_MULT,K_multiply_by_V_slepc,ierr)
@@ -546,8 +560,6 @@ subroutine K_multiply_by_V_slepc(slepc_mat,vi,vo,ierr)
  !
  implicit none
  !
- PetscFortranComplex   :: tmp_value(1)
- !
 #include <petsc/finclude/petscsys.h>
 #include <petsc/finclude/petscvec.h>
 #include <petsc/finclude/petscmat.h>
@@ -555,11 +567,16 @@ subroutine K_multiply_by_V_slepc(slepc_mat,vi,vo,ierr)
 #include <slepc/finclude/slepcsys.h>
 #include <slepc/finclude/slepceps.h>
  !
+ PetscFortranComplex  :: tmp_value(1)
+ PetscFortranInt      :: H_pos(1)
+ PetscErrorCode       :: ierr
+ !
  VecScatter           :: ctx
  PetscScalar, pointer :: xx(:)
  Vec                  :: vi,vo,x
  Mat                  :: slepc_mat
- integer              :: n, i_g, i_c, ierr, H_pos(1), start_index, low, high
+ !
+ integer              :: n, i_g, i_c, start_index, low, high
  !
  ! create scattering context vi (distributed) -> x (local)
  !

src/linear_algebra/MATRIX_slepc.F
@@ -50,10 +50,19 @@ subroutine MATRIX_slepc(A,n_eig,E_real,E_cmpl,V_right,V_left,&
   complex(SP),  optional      :: V_left(:,:)   ! left eigenvalues
   complex(SP),  optional      :: E_cmpl(:)     ! complex eigenvalues
   real(SP),     optional      :: E_real(:)     ! real eigenvalues
-  real(SP),     optional      :: target_energy ! energy near where to find the eigenvalues
-  real(SP),     optional      :: slepc_tol     ! tolerance of the iterative solver
+  !  real(SP),     optional      :: target_energy ! energy near where to find the eigenvalues
+  !  real(SP),     optional      :: slepc_tol     ! tolerance of the iterative solver
   character(schlen), optional :: extraction    ! extraction method for the eigenvalues
-  integer,optional            :: slepc_ncv     ! dimension of the subspace
+  !integer,optional            :: slepc_ncv     ! dimension of the subspace
+  !
+#if defined _DOUBLE
+  PetscFortranDouble, optional :: target_energy
+  PetscFortranDouble, optional :: slepc_tol
+#else
+  PetscFortranFloat, optional  :: target_energy
+  PetscFortranFloat, optional  :: slepc_tol
+#endif
+  PetscFortranInt,  optional   :: slepc_ncv
   ! 
   ! internal variables
   !
@@ -106,7 +115,7 @@ subroutine MATRIX_slepc(A,n_eig,E_real,E_cmpl,V_right,V_left,&
   call EPSSetProblemType(eps,EPS_HEP,ierr)
 
   if (target_energy .ne. 0) then
-    call msg( 'rs', 'criterion: target energy ', target_energy )
+    call msg( 'rs', 'criterion: target energy ', real(target_energy,SP) )
     call EPSSetTarget(eps,target_energy,ierr)
     call EPSSetWhichEigenpairs(eps,EPS_TARGET_REAL,ierr)
   else

src/modules/mod_interfaces.F
@@ -133,10 +133,19 @@ module interfaces
      complex(SP),  optional      :: V_left(:,:)  ! left eigenvalues
      complex(SP),  optional      :: E_cmpl(:)    ! complex eigenvalues
      real(SP),     optional      :: E_real(:)    ! real eigenvalues
-     real(SP),     optional      :: target_energy! energy near where to find the eigenvalues
-     real(SP),     optional      :: slepc_tol    ! tolerance of the iterative solver
+     !real(SP),     optional      :: target_energy! energy near where to find the eigenvalues
+     !real(SP),     optional      :: slepc_tol    ! tolerance of the iterative solver
      character(schlen), optional :: extraction   ! extraction method for the eigenvalues
-     integer,optional            :: slepc_ncv    ! dimension of the subspace
+     ! integer,optional            :: slepc_ncv    ! dimension of the subspace
+     !
+#if defined _DOUBLE
+     PetscFortranDouble, optional :: target_energy
+     PetscFortranDouble, optional :: slepc_tol
+#else
+     PetscFortranFloat, optional  :: target_energy
+     PetscFortranFloat, optional  :: slepc_tol
+#endif
+     PetscFortranInt,   optional  :: slepc_ncv
      !
    end subroutine
 #endif
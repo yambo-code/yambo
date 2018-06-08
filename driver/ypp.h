b72fe23a386c05a4877d5eec44d6697d9ceadca9
marini <andrea.marini@cnr.it>
2018-06-08 17:12:40 +0200

    Version 4.3.0,  Revision 15418,  Hash 55452ee
    MODIFIED *  config/version.m4 config/version.m4_gpl configure driver/ypp.h include/version.inc ypp/electrons/electrons_dos.F ypp/init/INIT_ypp.F ypp/modules/YPP_SET_defaults.F ypp/modules/mod_interpolate.F
    
    Additions:
    - [YPP] Energy interpolations implemented in the standard electronic DOS.
    
    Patch sent by:  marini <andrea.marini@cnr.it>

--- driver/ypp.h
+++ driver/ypp.h
@@ -80,9 +80,9 @@
   {"RTDBs",   "c","Carriers [(e)nergy,(k)-space]",0,0,1,0,0}, 
   {"RealTime","t","TD-polarization [(X)response,Tr(a)bs]",0,0,1,0,0}, 
   {"RealTime","n","NEQ plot opt#1 [(o)ccupations,(l)ifetimes,(d)ensity]",0,0,2,0,0}, 
-  {"DESC",    " ","         opt#2 occ. [(b)ands,(t)ime,(e)nergy,(d)os]",0,0,0,0,0},
-  {"DESC",    " ","         opt#2 life [(b)ands,(t)ime,(e)nergy]",0,0,0,0,0},
-  {"DESC",    " ","         opt#2 den. [(t)ime]",0,0,0,0,0}, 
+  {"DESC",    " ","         opt#1 o => opt#2 [(b)ands,(t)ime,(e)nergy,(d)os]",0,0,0,0,0},
+  {"DESC",    " ","         opt#1 l => opt#2 [(b)ands,(t)ime,(e)nergy]",0,0,0,0,0},
+  {"DESC",    " ","         opt#1 d => opt#2 [(t)ime]",0,0,0,0,0}, 
 #endif
 #if defined _YPP_ELPH && ! defined _YPP_RT
   {"phonons","p","Phonon [(d)os,(e)lias,(a)mplitude]",0,0,1,0,0}, 

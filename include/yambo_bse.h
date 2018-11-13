dbf211f3e8d17c834f3bf963497020741f425b12
Davide Sangalli <davide.sangalli@gmail.com>
2018-11-13 14:25:34 +0100

    Version 4.4.0,  Revision 16254,  Hash d3e01d12f
    MODIFIED *  config/version.m4 config/version.m4_gpl configure include/version.inc include/yambo_bse.h
    
    Changes:
    - [yambo] Removed comments from include/yambo_bse.h
              They were giving problems to intel pre-compiler
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/yambo_bse.h
+++ include/yambo_bse.h
@@ -29,85 +29,73 @@ integer ::     i_k_s,i_k_s_m1,i_p_s,i_k_bz,i_p_bz,i_k,i_p,i_kp_s,& NEWLINE \
 &               i_v_k,i_v_p,i_c_k,i_c_p,i_k_sp_pol,i_p_sp_pol,iq_W,iq_W_bz,iq_W_s,ig_W,& NEWLINE \
 &               i_kmq_t,i_pmq_t
 #define FILL_BSK_COMMON_INDEXES \
-     ! NEWLINE \
+     NEWLINE \
      H_pos(1) = sum(BS_T_grp(:I_Tgrp_k-1)%size)+i_Tk NEWLINE \
      H_pos(2) = sum(BS_T_grp(:I_Tgrp_p-1)%size)+i_Tp NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      if (H_pos(1)>H_pos(2)) cycle NEWLINE \
-     ! NEWLINE \
-     ! k and p indexes NEWLINE \
-     ! NEWLINE \
-     i_k_bz  = BS_T_grp(i_Tgrp_k)%table(i_Tk,1) ! k NEWLINE \
-     i_p_bz  = BS_T_grp(i_Tgrp_p)%table(i_Tp,1) ! p = k - q_W NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
+     i_k_bz  = BS_T_grp(i_Tgrp_k)%table(i_Tk,1) NEWLINE \
+     i_p_bz  = BS_T_grp(i_Tgrp_p)%table(i_Tp,1) NEWLINE \
+     NEWLINE \
      i_k_s =Xk%sstar(i_k_bz,2) NEWLINE \
      i_p_s =Xk%sstar(i_p_bz,2) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_k_s_m1= sop_inv(i_k_s) NEWLINE \
      i_kp_s  = sop_tab(i_k_s_m1,i_p_s) NEWLINE \
-     ! NEWLINE \
-     ! k-q and p-q indexes NEWLINE \
-     ! NEWLINE \
-     i_kmq_bz=qindx_X(iq,i_k_bz,1) ! k-q NEWLINE \
-     i_pmq_bz=qindx_X(iq,i_p_bz,1) ! p-q = k - q_W -q NEWLINE \
-     ! NEWLINE \
-     !ig_kmq=qindx_X(iq,i_k_bz,2) NEWLINE \
-     !ig_pmq=qindx_X(iq,i_p_bz,2) NEWLINE \
-     !ig_kp_mq=G_m_G(ig_kmq,ig_pmq) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
+     i_kmq_bz=qindx_X(iq,i_k_bz,1) NEWLINE \
+     i_pmq_bz=qindx_X(iq,i_p_bz,1) NEWLINE \
+     NEWLINE \
      i_kmq   =Xk%sstar(i_kmq_bz,1) NEWLINE \
      i_pmq   =Xk%sstar(i_pmq_bz,1) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_kmq_s =Xk%sstar(i_kmq_bz,2) NEWLINE \
      i_pmq_s =Xk%sstar(i_pmq_bz,2) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      if (BS_res_K_corr) then NEWLINE \
        i_kmq_t=BS_blk(i_block)%kp_table(1,i_kmq) NEWLINE \
        i_pmq_t=BS_blk(i_block)%kp_table(2,i_pmq) NEWLINE \
      endif NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_kmq_s_m1 = sop_inv(i_kmq_s) NEWLINE \
      i_kp_mq_s  = sop_tab(i_kmq_s_m1,i_pmq_s) NEWLINE \
-     ! NEWLINE \
-     ! q_W indexes NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      iq_W_bz=qindx_B(i_k_bz,i_p_bz,1) NEWLINE \
      ig_W   =qindx_B(i_k_bz,i_p_bz,2) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      iq_W   =q%sstar( iq_W_bz ,1) NEWLINE \
      iq_W_s =q%sstar( iq_W_bz ,2) NEWLINE \
-     ! NEWLINE \
-     ! Bands and spin indexes NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_v_k=BS_T_grp(i_Tgrp_k)%table(i_Tk,2) NEWLINE \
      i_c_k=BS_T_grp(i_Tgrp_k)%table(i_Tk,3) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_v_p=BS_T_grp(i_Tgrp_p)%table(i_Tp,2) NEWLINE \
      i_c_p=BS_T_grp(i_Tgrp_p)%table(i_Tp,3) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      i_k_sp_pol=BS_T_grp(i_Tgrp_k)%table(i_Tk,4) NEWLINE \
      i_p_sp_pol=BS_T_grp(i_Tgrp_p)%table(i_Tp,4) NEWLINE \
-     !
+
 #define FILL_BSK_KERNEL_INDEXES \
-     ! NEWLINE \
-     ig_kmq    = qindx_X(iq,i_k_bz,2) ! Go shift to reach the first BZ NEWLINE \
+     NEWLINE \
+     ig_kmq    = qindx_X(iq,i_k_bz,2) NEWLINE \
      ig_pmq    = qindx_X(iq,i_p_bz,2) NEWLINE \
-     ! NEWLINE \
-     iq_W_bz_mq=qindx_B(i_kmq_bz,i_pmq_bz,1) !q_W_mq_bz+G_W_mq=kmq_bz-pmq_bz NEWLINE \
+     NEWLINE \
+     iq_W_bz_mq=qindx_B(i_kmq_bz,i_pmq_bz,1) NEWLINE \
      ig_W_mq   =qindx_B(i_kmq_bz,i_pmq_bz,2) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      iq_W_mq   =q%sstar( iq_W_bz_mq,1) NEWLINE \
      iq_W_s_mq =q%sstar( iq_W_bz_mq,2) NEWLINE \
-     ! NEWLINE \
+     NEWLINE \
      if (BS_K_is_ALDA) then NEWLINE \
        is_k = (/i_c_k,i_k,i_k_s,i_k_sp_pol/) NEWLINE \
        os_k = (/i_v_k,i_kmq,i_kmq_s,i_k_sp_pol/) NEWLINE \
        is_p = (/i_c_p,i_p,i_p_s,i_p_sp_pol/) NEWLINE \
        os_p = (/i_v_p,i_pmq,i_pmq_s,i_p_sp_pol/) NEWLINE \
      endif NEWLINE \
-     !
+
 #define FILL_BSK_CORR_INDEXES \
-     ! NEWLINE \
+     NEWLINE \
      i_s_star=Xk%s_table(i_p,i_kp_s) NEWLINE \
      i_s_mq_star=Xk%s_table(i_pmq,i_kp_mq_s) NEWLINE \
-     !
+

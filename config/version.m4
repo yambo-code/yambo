yambo-code/yambo

commit ecd803e6a5a4680511161794ddf2f4fe019a44fc

Version 4.5.0,  Revision 17605,  Hash 0881e328c
MODIFIED *  config/version.m4 config/version.m4_gpl configure pol_function/X_dielectric_matrix.F

Bugs:
- [yambo] Mostly aeshetic fix

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 4.5.0 r.17604 h.c62a57833, yambo@yambo-code.org)
+AC_INIT(Yambo, 4.5.0 r.17605 h.0881e328c, yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
-SREVISION="17604"
-SHASH="c62a57833"
+SREVISION="17605"
+SHASH="0881e328c"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.4.0 r.108 (based on r.17604 h.c62a57833), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.4.0 r.108 (based on r.17605 h.0881e328c), yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="17604"
-SHASH="c62a57833"
+BASE_REV="17605"
+SHASH="0881e328c"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 4.5.0 r.17604 h.c62a57833.
+# Generated by GNU Autoconf 2.69 for Yambo 4.5.0 r.17605 h.0881e328c.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='4.5.0 r.17604 h.c62a57833'
-PACKAGE_STRING='Yambo 4.5.0 r.17604 h.c62a57833'
+PACKAGE_VERSION='4.5.0 r.17605 h.0881e328c'
+PACKAGE_STRING='Yambo 4.5.0 r.17605 h.0881e328c'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1501,7 +1501,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 4.5.0 r.17604 h.c62a57833 to adapt to many kinds of systems.
+\`configure' configures Yambo 4.5.0 r.17605 h.0881e328c to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1567,7 +1567,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 4.5.0 r.17604 h.c62a57833:";;
+     short | recursive ) echo "Configuration of Yambo 4.5.0 r.17605 h.0881e328c:";;
    esac
   cat <<\_ACEOF
 
@@ -1760,7 +1760,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 4.5.0 r.17604 h.c62a57833
+Yambo configure 4.5.0 r.17605 h.0881e328c
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2439,7 +2439,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 4.5.0 r.17604 h.c62a57833, which was
+It was created by Yambo $as_me 4.5.0 r.17605 h.0881e328c, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2790,8 +2790,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
-SREVISION="17604"
-SHASH="c62a57833"
+SREVISION="17605"
+SHASH="0881e328c"
 
 
 
@@ -14339,7 +14339,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 4.5.0 r.17604 h.c62a57833, which was
+This file was extended by Yambo $as_me 4.5.0 r.17605 h.0881e328c, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -14401,7 +14401,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 4.5.0 r.17604 h.c62a57833
+Yambo config.status 4.5.0 r.17605 h.0881e328c
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

src/pol_function/X_dielectric_matrix.F
@@ -204,6 +204,13 @@ integer function X_dielectric_matrix(Xen,Xk,q,X,Xw,Dip,SILENT_MODE)
    !
  endif
  !
+ if (i_err/=0) then
+   call io_control(ACTION=OP_WR_CL,COM=REP,SEC=(/1/),  ID=Xdb)
+   i_err=io_X(X,Xw,Xdb)
+ endif
+ !
+ call PP_redux_wait()
+ !
  do iq=X%iq(1),X%iq(2)
    !
    if (     io_X_done(iq) > 0       ) cycle
@@ -225,11 +232,6 @@ integer function X_dielectric_matrix(Xen,Xk,q,X,Xw,Dip,SILENT_MODE)
      !
      call TDDFT_ALDA_G_space(Xen,Xk)
      !
-   endif
-   !
-   if(iq==1) then
-     call io_control(ACTION=OP_WR_CL,COM=REP,SEC=(/1/),  ID=Xdb)
-     i_err=io_X(X,Xw,Xdb)
    endif
    !
    ! Allocation

commit 0adde2f15b480f387d11553586df0513df208261

Version 4.5.0,  Revision 17759,  Hash d8513521d
MODIFIED *  config/version.m4 config/version.m4_gpl configure pol_function/X_dielectric_matrix.F

Bugs:
- [yambo] Fixed again bug introduced with commit ecd803e6a5a4680511161794ddf2f4fe019a44fc
          See also commit 78c79d45af3073fa47f928c84aa9388b166e04be
          The fix was erased with the merge  of branch devel-sc

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 4.5.0 r.17755 h.a71278428, yambo@yambo-code.org)
+AC_INIT(Yambo, 4.5.0 r.17759 h.d8513521d, yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
-SREVISION="17755"
-SHASH="a71278428"
+SREVISION="17759"
+SHASH="d8513521d"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.4.0 r.108 (based on r.17755 h.a71278428), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.4.0 r.108 (based on r.17759 h.d8513521d), yambo@yambo-code.org)
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="17755"
-SHASH="a71278428"
+BASE_REV="17759"
+SHASH="d8513521d"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 4.5.0 r.17755 h.a71278428.
+# Generated by GNU Autoconf 2.69 for Yambo 4.5.0 r.17759 h.d8513521d.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='4.5.0 r.17755 h.a71278428'
-PACKAGE_STRING='Yambo 4.5.0 r.17755 h.a71278428'
+PACKAGE_VERSION='4.5.0 r.17759 h.d8513521d'
+PACKAGE_STRING='Yambo 4.5.0 r.17759 h.d8513521d'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1522,7 +1522,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 4.5.0 r.17755 h.a71278428 to adapt to many kinds of systems.
+\`configure' configures Yambo 4.5.0 r.17759 h.d8513521d to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1588,7 +1588,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 4.5.0 r.17755 h.a71278428:";;
+     short | recursive ) echo "Configuration of Yambo 4.5.0 r.17759 h.d8513521d:";;
    esac
   cat <<\_ACEOF
 
@@ -1792,7 +1792,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 4.5.0 r.17755 h.a71278428
+Yambo configure 4.5.0 r.17759 h.d8513521d
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2471,7 +2471,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 4.5.0 r.17755 h.a71278428, which was
+It was created by Yambo $as_me 4.5.0 r.17759 h.d8513521d, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2822,8 +2822,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="4"
 SSUBVERSION="5"
 SPATCHLEVEL="0"
-SREVISION="17755"
-SHASH="a71278428"
+SREVISION="17759"
+SHASH="d8513521d"
 
 
 
@@ -14690,7 +14690,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 4.5.0 r.17755 h.a71278428, which was
+This file was extended by Yambo $as_me 4.5.0 r.17759 h.d8513521d, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -14752,7 +14752,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 4.5.0 r.17755 h.a71278428
+Yambo config.status 4.5.0 r.17759 h.d8513521d
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

src/pol_function/X_dielectric_matrix.F
@@ -36,7 +36,7 @@ integer function X_dielectric_matrix(Xen,Xk,q,X,Xw,Dip,SILENT_MODE)
  use electrons,     ONLY:levels
  use parallel_m,    ONLY:PP_redux_wait,PAR_IND_Q,PAR_Q_index, PAR_nq, &
 &                        PAR_IND_Xk_ibz,PAR_IND_CON_BANDS_X,PAR_IND_VAL_BANDS_X,PAR_COM_X_WORLD,&
-&                        PAR_COM_X_WORLD_RL_resolved,master_cpu
+&                        PAR_COM_X_WORLD_RL_resolved
  use wave_func,     ONLY:WF_buffered_IO,WF,WF_buffer
  use IO_m,          ONLY:io_control,OP_RD_CL,OP_APP_CL,VERIFY,REP,io_RESPONSE,OP_WR_CL
  use com,           ONLY:depth
@@ -201,14 +201,6 @@ integer function X_dielectric_matrix(Xen,Xk,q,X,Xw,Dip,SILENT_MODE)
    !
  endif
  !
- ! AM. Dec 2019. master_cpu below is needed because when the RL vectors are parallelized
- ! more than 1 cpu (beside the master) has WRITING rights. But only 1 CPU must write the DB header.
- !
- if (i_err/=0.and.master_cpu) then
-   call io_control(ACTION=OP_WR_CL,COM=REP,SEC=(/1/),  ID=Xdb)
-   i_err=io_X(X,Xw,Xdb)
- endif
- !
  call PP_redux_wait()
  !
  do iq=X%iq(1),X%iq(2)
@@ -232,6 +224,11 @@ integer function X_dielectric_matrix(Xen,Xk,q,X,Xw,Dip,SILENT_MODE)
      !
      call TDDFT_ALDA_G_space(Xen,Xk)
      !
+   endif
+   !
+   if (iq==1) then
+     call io_control(ACTION=OP_WR_CL,COM=REP,SEC=(/1/),ID=Xdb)
+     i_err=io_X(X,Xw,Xdb)
    endif
    !
    ! Allocation
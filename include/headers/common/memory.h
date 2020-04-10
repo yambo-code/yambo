a9b26d4991028923e15049cfa57314e7c39c858a
Davide Sangalli <davide.sangalli@gmail.com>
2020-04-11 01:09:23 +0200

    Version 4.5.0,  Revision 18069,  Hash 2c2c37dff
    MODIFIED *  config/version.m4 config/version.m4_gpl configure include/headers/common/memory.h io/io_BS_PAR.F memory/MEM_manager.F modules/.objects modules/SET_defaults.F modules/mod_memory.F modules/mod_parallel.F parallel/PARALLEL_structure.F
    
    NEW *  modules/mod_lexical_sort.F
    
    Bugs:
    i-
    
    Additions:
    -
    
    Changes:
    - [yambo] Working on issue #19
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/headers/common/memory.h
+++ include/headers/common/memory.h
@@ -22,21 +22,38 @@
  MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
  
 */
- use memory, ONLY:MEM_err,MEM_dri,IPL
+ use memory, ONLY:MEM_err,MEM_msg,MEM_count,IPL
  implicit none
+
 #define YAMBO_ALLOC_P(x,SIZE) \
-  allocate(x SIZE,stat=MEM_err)NEWLINE \
-  if (     associated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,x)NEWLINE \
-  if (.not.associated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES)
+  allocate(x SIZE, &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
+  if (     associated(x)) &NEWLINE& call MEM_count(QUOTES x QUOTES,x)NEWLINE \
+  if (.not.associated(x)) &NEWLINE& call MEM_error(QUOTES x QUOTES)
 #define YAMBO_ALLOC(x,SIZE) \
-  allocate(x SIZE,stat=MEM_err)NEWLINE \
-  if (     allocated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,x)NEWLINE \
-  if (.not.allocated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES)
+  allocate(x SIZE,  &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
+  if (     allocated(x)) &NEWLINE& call MEM_count(QUOTES x QUOTES,x)NEWLINE \
+  if (.not.allocated(x)) &NEWLINE& call MEM_error(QUOTES x QUOTES)
+
 #define YAMBO_FREE(x) \
-  if (.not.allocated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
-  if (     allocated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
+  if (.not.allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
+  if (     allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
   if (     allocated(x)) &NEWLINE& deallocate(x)
 #define YAMBO_FREE_P(x) \
-  if (.not.associated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
-  if (     associated(x)) &NEWLINE& call MEM_dri(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
+  if (.not.associated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
+  if (     associated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
   if (     associated(x)) &NEWLINE& deallocate(x);nullify(x)
+
+#if defined _MPI
+#define YAMBO_PAR_ALLOC(x,SIZE) \
+  if (PAR_COM_HOST%n_CPU>1) then NEWLINE \
+    HOST_SIZE=SIZE NEWLINE \
+    call PP_redux_wait(HOST_SIZE,COMM=PAR_COM_HOST%COMM) NEWLINE \
+    if (.and.PAR_COM_HOST%CPU_id==0 ) then NEWLINE \
+      allocate(x HOST_SIZE,stat=MEM_err,errmsg=MEM_msg)  NEWLINE \
+      call MEM_global_msg(QUOTES x QUOTES) NEWLINE \
+      deallocate(x) NEWLINE \
+    endif NEWLINE \
+    call PP_redux_wait(COMM=PAR_COM_HOST%COMM)  NEWLINE \
+  endif NEWLINE \
+  YAMBO_ALLOC(x,SIZE)
+#endif

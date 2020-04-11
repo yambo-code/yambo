7cd576232945877929a1d36dc381b9b09ac8c6e4
Davide Sangalli <davide.sangalli@gmail.com>
2020-04-12 00:05:13 +0200

    Version 4.5.0,  Revision 18070,  Hash a9b26d499
    MODIFIED *  config/version.m4 config/version.m4_gpl configure include/headers/common/memory.h bse/K_blocks.F memory/MEM_manager.F modules/mod_memory.F parallel/PARALLEL_structure.F
    
    Changes:
    - [yambo] First version of solution to issue #19 implemented
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/headers/common/memory.h
+++ include/headers/common/memory.h
@@ -22,8 +22,87 @@
  MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
  
 */
- use memory, ONLY:MEM_err,MEM_msg,MEM_count,IPL
+ use memory, ONLY:MEM_err,MEM_msg,MEM_count,MEM_global_mesg,IPL
  implicit none
+
+#if defined _MPI
+
+#define CHECK_ALLOC_A(SIZE) \
+  if (PAR_COM_HOST%n_CPU>1) then NEWLINE \
+    LOCAL_SIZE=SIZE NEWLINE \
+    HOST_SIZE=SIZE NEWLINE \
+    call PP_redux_wait(HOST_SIZE,COMM=PAR_COM_HOST%COMM) NEWLINE \
+    if (PAR_COM_HOST%CPU_id==0 ) then
+
+#define CHECK_ALLOC_B(x,HOST_SIZE) \
+      call MEM_global_mesg(QUOTES x QUOTES,HOST_SIZE) NEWLINE \
+      deallocate(x) NEWLINE \
+    endif NEWLINE \
+    call PP_redux_wait(COMM=PAR_COM_HOST%COMM)  NEWLINE \
+  endif
+
+#define YAMBO_PAR_ALLOC1(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC1(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC1(x,LOCAL_SIZE)
+#define YAMBO_PAR_ALLOC2(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC2(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC2(x,LOCAL_SIZE)
+#define YAMBO_PAR_ALLOC3(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC3(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC3(x,LOCAL_SIZE)
+#define YAMBO_PAR_ALLOC4(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC4(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC4(x,LOCAL_SIZE)
+#define YAMBO_PAR_ALLOC5(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC5(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC5(x,LOCAL_SIZE)
+#define YAMBO_PAR_ALLOC6(x,SIZE) \
+  CHECK_ALLOC_A(SIZE) NEWLINE \
+  SIMPLE_ALLOC6(x,HOST_SIZE) NEWLINE \
+  CHECK_ALLOC_B(x,HOST_SIZE) NEWLINE \
+  YAMBO_ALLOC6(x,LOCAL_SIZE)
+
+
+#define SIMPLE_ALLOC1(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1)))
+#define SIMPLE_ALLOC2(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1),SIZE(2)))
+#define SIMPLE_ALLOC3(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3)))
+#define SIMPLE_ALLOC4(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4)))
+#define SIMPLE_ALLOC5(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4),SIZE(5)))
+#define SIMPLE_ALLOC6(x,SIZE) \
+    SIMPLE_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4),SIZE(5),SIZE(6)))
+
+#define YAMBO_ALLOC1(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1)))
+#define YAMBO_ALLOC2(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1),SIZE(2)))
+#define YAMBO_ALLOC3(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3)))
+#define YAMBO_ALLOC4(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4)))
+#define YAMBO_ALLOC5(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4),SIZE(5)))
+#define YAMBO_ALLOC6(x,SIZE) \
+    YAMBO_ALLOC(x,(SIZE(1),SIZE(2),SIZE(3),SIZE(4),SIZE(5),SIZE(6)))
+
+#define SIMPLE_ALLOC(x,SIZE) \
+  allocate(x SIZE, &NEWLINE& stat=MEM_err,errmsg=MEM_msg)  NEWLINE \
+
+#endif
 
 #define YAMBO_ALLOC_P(x,SIZE) \
   allocate(x SIZE, &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
@@ -43,17 +122,4 @@
   if (     associated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
   if (     associated(x)) &NEWLINE& deallocate(x);nullify(x)
 
-#if defined _MPI
-#define YAMBO_PAR_ALLOC(x,SIZE) \
-  if (PAR_COM_HOST%n_CPU>1) then NEWLINE \
-    HOST_SIZE=SIZE NEWLINE \
-    call PP_redux_wait(HOST_SIZE,COMM=PAR_COM_HOST%COMM) NEWLINE \
-    if (.and.PAR_COM_HOST%CPU_id==0 ) then NEWLINE \
-      allocate(x HOST_SIZE,stat=MEM_err,errmsg=MEM_msg)  NEWLINE \
-      call MEM_global_msg(QUOTES x QUOTES) NEWLINE \
-      deallocate(x) NEWLINE \
-    endif NEWLINE \
-    call PP_redux_wait(COMM=PAR_COM_HOST%COMM)  NEWLINE \
-  endif NEWLINE \
-  YAMBO_ALLOC(x,SIZE)
-#endif
+

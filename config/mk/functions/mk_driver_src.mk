5ad86045be3ea04d599571928f35c0ccb5faf80a
Andrea Marini <andrea.marini@cnr.it>
2020-05-27 13:20:07 +0200

    Version 4.5.0,  Revision 18376,  Hash 34916408c
    MODIFIED *  .gitignore config/configure.ac config/mk/actions/compile_yambo.mk config/mk/functions/mk_driver_src.mk config/mk/variables.mk config/version/version.m4 config/version/version.m4_gpl configure driver/.objects sbin/make_makefile.sh.in
    
    DELETED *  lib/yambo/driver/.empty
    
    RENAMED *  driver/driver.c -> lib/yambo/driver/driver.c driver/include/driver.h -> lib/yambo/driver/include/driver.h driver/include/editor.h.in -> lib/yambo/driver/include/editor.h.in driver/include/fortran_arguments.h -> lib/yambo/driver/include/fortran_arguments.h driver/include/fortran_driver.h -> lib/yambo/driver/include/fortran_driver.h driver/include/kind.h -> lib/yambo/driver/include/kind.h driver/include/tool.h -> lib/yambo/driver/include/tool.h driver/include/version.h.in -> lib/yambo/driver/include/version.h.in driver/interface/.objects -> lib/yambo/driver/interface/.objects driver/interface/C_driver_transfer.F -> lib/yambo/driver/interface/C_driver_transfer.F driver/interface/get_libraries.c -> lib/yambo/driver/interface/get_libraries.c driver/interface/get_runlevel.c -> lib/yambo/driver/interface/get_runlevel.c driver/interface/get_running_project.c -> lib/yambo/driver/interface/get_running_project.c driver/interface/get_running_tool.c -> lib/yambo/driver/interface/get_running_tool.c driver/interface/get_version.c -> lib/yambo/driver/interface/get_version.c driver/interface/mod_C_driver.F -> lib/yambo/driver/interface/mod_C_driver.F driver/lib/.objects -> lib/yambo/driver/lib/.objects driver/lib/command_line.c -> lib/yambo/driver/lib/command_line.c driver/lib/input_file.c -> lib/yambo/driver/lib/input_file.c driver/lib/launcher.c -> lib/yambo/driver/lib/launcher.c driver/lib/load_environments.c -> lib/yambo/driver/lib/load_environments.c driver/lib/options_maker.c -> lib/yambo/driver/lib/options_maker.c driver/lib/title.c -> lib/yambo/driver/lib/title.c driver/lib/tool_init.c -> lib/yambo/driver/lib/tool_init.c driver/lib/usage.c -> lib/yambo/driver/lib/usage.c driver/lib/use_me.c -> lib/yambo/driver/lib/use_me.c driver/options/.objects -> lib/yambo/driver/options/.objects driver/options/options_control.c -> lib/yambo/driver/options/options_control.c driver/options/options_help.c -> lib/yambo/driver/options/options_help.c driver/options/options_interfaces.c -> lib/yambo/driver/options/options_interfaces.c driver/options/options_projects.c -> lib/yambo/driver/options/options_projects.c driver/options/options_yambo.c -> lib/yambo/driver/options/options_yambo.c driver/options/options_ypp.c -> lib/yambo/driver/options/options_ypp.c
    
    Changes:
    - driver -> lib/yambo/driver
    
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- config/mk/functions/mk_driver_src.mk
+++ config/mk/functions/mk_driver_src.mk
@@ -1,16 +1,16 @@
 define mk_driver_src
- cp -r $(topdir)/driver/include/*.h include/ ;\
- for ldir in $$LIBS2DO; do \
+ cp -r $(topdir)/lib/yambo/driver/include/*.h include/ ;\
+ for ldir in $(DRIVER_LIBS); do \
   if test ! -f "$(libdir)/libdriver_$$ldir.a" || test "$(keep_objs)" = yes  ; then \
    rm -f "$(libdir)/libdriver_$$ldir.a" ; \
    echo " " ; \
-   echo ">>>[Making driver/$$ldir]<<<" ; \
-   if test ! -d "$$XPATH/$$ldir" ; then mkdir -p "$$XPATH/$$ldir" ; fi ; \
-   if [ "$(topdir)" != "$(prefix)" ] && [ -f $$VPATH/$$ldir/.objects ] ; then \
-    cp $$VPATH/$$ldir/.objects $$XPATH/$$ldir ; \
-   fi ; \
-   ./sbin/make_makefile.sh $$XPATH/$$ldir libdriver_$$ldir.a .objects l $(xcpp) $$ADF ; \
-   cd $$XPATH/$$ldir ; $(MAKE) VPATH=$$VPATH/$$ldir || exit "$$?" ; cd ../../; \
+   echo ">>>[Making lib/yambo/driver/$$ldir]<<<" ; \
+   if test ! -d "lib/yambo/driver/$$ldir" ; then mkdir -p "lib/yambo/driver/$$ldir" ; fi ; \
+   if [ "$(topdir)" != "$(prefix)" ] && [ -f $(topdir)/lib/yambo/driver/$$ldir/.objects ] ; then \
+    cp $(topdir)/lib/yambo/driver/$$ldir/.objects lib/yambo/driver/$$ldir ; \
+   fi ;pwd; \
+   ./sbin/make_makefile.sh lib/yambo/driver/$$ldir libdriver_$$ldir.a .objects l $(xcpp) $$ADF ; \
+   cd lib/yambo/driver/$$ldir ; $(MAKE) VPATH=$(topdir)/lib/yambo/driver/$$ldir || exit "$$?" ; cd ../../../../; \
   fi ; \
  done
 endef

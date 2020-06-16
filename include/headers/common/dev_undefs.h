cfbb27a4a600b7491086431baf969849a7e76db9
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2020-06-16 12:10:29 +0200

    deviceXlib updated

---
+++ include/headers/common/dev_undefs.h
@@ -0,0 +1,37 @@
+/*
+        Copyright (C) 2000-2020 the YAMBO team
+              http://www.yambo-code.org
+
+ Authors (see AUTHORS file for details): AF
+ 
+ This file is distributed under the terms of the GNU 
+ General Public License. You can redistribute it and/or 
+ modify it under the terms of the GNU General Public 
+ License as published by the Free Software Foundation; 
+ either version 2, or (at your option) any later version.
+
+ This program is distributed in the hope that it will 
+ be useful, but WITHOUT ANY WARRANTY; without even the 
+ implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ PARTICULAR PURPOSE.  See the GNU General Public License 
+ for more details.
+
+ You should have received a copy of the GNU General Public 
+ License along with this program; if not, write to the Free 
+ Software Foundation, Inc., 59 Temple Place - Suite 330,Boston, 
+ MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
+*/
+
+
+#undef DEV_SUBNAME(x)
+#undef DEV_SUBNAME_ALT(x)
+#undef DEV_VARNAME(x)
+#undef DEV_ATTRIBUTE
+#undef DEV_PINNED
+
+#undef DEV_SUB(x)
+#undef DEV_SUB_ALT(x)
+#undef DEV_VAR(x)
+#undef DEV_ATTR
+#undef DEV_PIN
+

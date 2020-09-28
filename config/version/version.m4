yambo-code/yambo

commit 53d4ce8193d55c4266e68ae0173946048936053a

Version 5.0.1,  Revision 18782,  Hash 08b1bb63e
MODIFIED *  config/version/version.m4 config/version/version.m4_gpl configure common/OCCUPATIONS_Fermi.F

Bugs:
- [yambo] Fix for issue #363

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 5.0.1 r.18779 h.0d3a835f3, yambo@yambo-code.org)
+AC_INIT(Yambo, 5.0.1 r.18782 h.08b1bb63e, yambo@yambo-code.org)
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="1"
-SREVISION="18779"
-SHASH="0d3a835f3"
+SREVISION="18782"
+SHASH="08b1bb63e"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.4.0 r.108 (based on r.18779 h.0d3a835f3), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.4.0 r.108 (based on r.18782 h.08b1bb63e), yambo@yambo-code.org)
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="18779"
-SHASH="0d3a835f3"
+BASE_REV="18782"
+SHASH="08b1bb63e"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 5.0.1 r.18779 h.0d3a835f3.
+# Generated by GNU Autoconf 2.69 for Yambo 5.0.1 r.18782 h.08b1bb63e.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='5.0.1 r.18779 h.0d3a835f3'
-PACKAGE_STRING='Yambo 5.0.1 r.18779 h.0d3a835f3'
+PACKAGE_VERSION='5.0.1 r.18782 h.08b1bb63e'
+PACKAGE_STRING='Yambo 5.0.1 r.18782 h.08b1bb63e'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1527,7 +1527,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 5.0.1 r.18779 h.0d3a835f3 to adapt to many kinds of systems.
+\`configure' configures Yambo 5.0.1 r.18782 h.08b1bb63e to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1593,7 +1593,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 5.0.1 r.18779 h.0d3a835f3:";;
+     short | recursive ) echo "Configuration of Yambo 5.0.1 r.18782 h.08b1bb63e:";;
    esac
   cat <<\_ACEOF
 
@@ -1797,7 +1797,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 5.0.1 r.18779 h.0d3a835f3
+Yambo configure 5.0.1 r.18782 h.08b1bb63e
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2476,7 +2476,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 5.0.1 r.18779 h.0d3a835f3, which was
+It was created by Yambo $as_me 5.0.1 r.18782 h.08b1bb63e, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2827,8 +2827,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="1"
-SREVISION="18779"
-SHASH="0d3a835f3"
+SREVISION="18782"
+SHASH="08b1bb63e"
 
 
 
@@ -14795,7 +14795,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 5.0.1 r.18779 h.0d3a835f3, which was
+This file was extended by Yambo $as_me 5.0.1 r.18782 h.08b1bb63e, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -14857,7 +14857,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 5.0.1 r.18779 h.0d3a835f3
+Yambo config.status 5.0.1 r.18782 h.08b1bb63e
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

src/common/OCCUPATIONS_Fermi.F
@@ -190,8 +190,8 @@ subroutine OCCUPATIONS_Fermi(E,K,OBJ,mode,n_bands)
    enddo
    if (i_Ef(1)/=i_Ef(2).or.i_Ef(1)/=i_Ef_fine(2)) then
      Tel=Tel+Tel_step
-     i_Ef(1)=i_Ef(1)-2
-     i_Ef(2)=i_Ef(2)+2
+     i_Ef(1)=max(1,i_Ef(1)-2)
+     i_Ef(2)=min(i_Ef(2)+2,E%nb)
    endif
    !
    ! 5. Use a secant-like procedure to converge to the final Level

commit 48b5dadbed6e44883807879a57bd37dfd8ffdcf2

Version 5.0.1,  Revision 18784,  Hash 9892392e9
MODIFIED *  config/version/version.m4 config/version/version.m4_gpl configure common/OCCUPATIONS_Fermi.F

Bugs:
- Fixed problem introduced with commit 53d4ce8193d55c4266e68ae0173946048936053a

Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

config/version/version.m4
@@ -1,9 +1,9 @@
-AC_INIT(Yambo, 5.0.1 r.18782 h.08b1bb63e, yambo@yambo-code.org)
+AC_INIT(Yambo, 5.0.1 r.18785 h.9892392e9, yambo@yambo-code.org)
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="1"
-SREVISION="18782"
-SHASH="08b1bb63e"
+SREVISION="18785"
+SHASH="9892392e9"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

config/version/version.m4_gpl
@@ -1,10 +1,10 @@
-AC_INIT(Yambo, 4.4.0 r.108 (based on r.18782 h.08b1bb63e), yambo@yambo-code.org)
+AC_INIT(Yambo, 4.4.0 r.108 (based on r.18785 h.9892392e9), yambo@yambo-code.org)
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="0"
 SREVISION="108"
-BASE_REV="18782"
-SHASH="08b1bb63e"
+BASE_REV="18785"
+SHASH="9892392e9"
 AC_SUBST(SVERSION)
 AC_SUBST(SSUBVERSION)
 AC_SUBST(SPATCHLEVEL)

configure
@@ -1,6 +1,6 @@
 #! /bin/sh
 # Guess values for system-dependent variables and create Makefiles.
-# Generated by GNU Autoconf 2.69 for Yambo 5.0.1 r.18782 h.08b1bb63e.
+# Generated by GNU Autoconf 2.69 for Yambo 5.0.1 r.18785 h.9892392e9.
 #
 # Report bugs to <yambo@yambo-code.org>.
 #
@@ -580,8 +580,8 @@ MAKEFLAGS=
 # Identity of this package.
 PACKAGE_NAME='Yambo'
 PACKAGE_TARNAME='yambo'
-PACKAGE_VERSION='5.0.1 r.18782 h.08b1bb63e'
-PACKAGE_STRING='Yambo 5.0.1 r.18782 h.08b1bb63e'
+PACKAGE_VERSION='5.0.1 r.18785 h.9892392e9'
+PACKAGE_STRING='Yambo 5.0.1 r.18785 h.9892392e9'
 PACKAGE_BUGREPORT='yambo@yambo-code.org'
 PACKAGE_URL=''
 
@@ -1527,7 +1527,7 @@ if test "$ac_init_help" = "long"; then
   # Omit some internal or obsolete options to make the list less imposing.
   # This message is too long to be a string in the A/UX 3.1 sh.
   cat <<_ACEOF
-\`configure' configures Yambo 5.0.1 r.18782 h.08b1bb63e to adapt to many kinds of systems.
+\`configure' configures Yambo 5.0.1 r.18785 h.9892392e9 to adapt to many kinds of systems.
 
 Usage: $0 [OPTION]... [VAR=VALUE]...
 
@@ -1593,7 +1593,7 @@ fi
 
 if test -n "$ac_init_help"; then
   case $ac_init_help in
-     short | recursive ) echo "Configuration of Yambo 5.0.1 r.18782 h.08b1bb63e:";;
+     short | recursive ) echo "Configuration of Yambo 5.0.1 r.18785 h.9892392e9:";;
    esac
   cat <<\_ACEOF
 
@@ -1797,7 +1797,7 @@ fi
 test -n "$ac_init_help" && exit $ac_status
 if $ac_init_version; then
   cat <<\_ACEOF
-Yambo configure 5.0.1 r.18782 h.08b1bb63e
+Yambo configure 5.0.1 r.18785 h.9892392e9
 generated by GNU Autoconf 2.69
 
 Copyright (C) 2012 Free Software Foundation, Inc.
@@ -2476,7 +2476,7 @@ cat >config.log <<_ACEOF
 This file contains any messages produced by compilers while
 running configure, to aid debugging if configure makes a mistake.
 
-It was created by Yambo $as_me 5.0.1 r.18782 h.08b1bb63e, which was
+It was created by Yambo $as_me 5.0.1 r.18785 h.9892392e9, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   $ $0 $@
@@ -2827,8 +2827,8 @@ ac_compiler_gnu=$ac_cv_c_compiler_gnu
 SVERSION="5"
 SSUBVERSION="0"
 SPATCHLEVEL="1"
-SREVISION="18782"
-SHASH="08b1bb63e"
+SREVISION="18785"
+SHASH="9892392e9"
 
 
 
@@ -14795,7 +14795,7 @@ cat >>$CONFIG_STATUS <<\_ACEOF || ac_write_fail=1
 # report actual input values of CONFIG_FILES etc. instead of their
 # values after options handling.
 ac_log="
-This file was extended by Yambo $as_me 5.0.1 r.18782 h.08b1bb63e, which was
+This file was extended by Yambo $as_me 5.0.1 r.18785 h.9892392e9, which was
 generated by GNU Autoconf 2.69.  Invocation command line was
 
   CONFIG_FILES    = $CONFIG_FILES
@@ -14857,7 +14857,7 @@ _ACEOF
 cat >>$CONFIG_STATUS <<_ACEOF || ac_write_fail=1
 ac_cs_config="`$as_echo "$ac_configure_args" | sed 's/^ //; s/[\\""\`\$]/\\\\&/g'`"
 ac_cs_version="\\
-Yambo config.status 5.0.1 r.18782 h.08b1bb63e
+Yambo config.status 5.0.1 r.18785 h.9892392e9
 configured by $0, generated by GNU Autoconf 2.69,
   with options \\"\$ac_cs_config\\"
 

src/common/OCCUPATIONS_Fermi.F
@@ -191,7 +191,7 @@ subroutine OCCUPATIONS_Fermi(E,K,OBJ,mode,n_bands)
    if (i_Ef(1)/=i_Ef(2).or.i_Ef(1)/=i_Ef_fine(2)) then
      Tel=Tel+Tel_step
      i_Ef(1)=max(1,i_Ef(1)-2)
-     i_Ef(2)=min(i_Ef(2)+2,E%nb)
+     i_Ef(2)=min(i_Ef(2)+2,n_total_states)
    endif
    !
    ! 5. Use a secant-like procedure to converge to the final Level
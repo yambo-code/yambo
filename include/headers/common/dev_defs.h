79a3bc8ea8be07236d2e0c737004ffae1b50a04a
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2020-11-17 12:45:34 +0100

    QP_ppa_cohsex cleanup; GPU porting of G-terminator implemented and tested

--- include/headers/common/dev_defs.h
+++ include/headers/common/dev_defs.h
@@ -22,6 +22,7 @@
  MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
 */
 
+#include <device_macros.h>
 
 #ifdef __STDC__
 #  define CAT(a,b) a##b 
@@ -50,11 +51,22 @@
 #define DEV_ATTR            DEV_ATTRIBUTE
 #define DEV_PIN             DEV_PINNED
 
-!#ifdef CUDA
-!  #define YAMBO_CUDA_OR_OMP(priv_list,nloop)  !$cuf kernel do(nloop) <<<*,*>>>
-!  #define YAMBO_CUDA_OR_OMP_END
-!#else 
-!  #define YAMBO_CUDA_OR_OMP(priv_list,nloop)  !$omp parallel do default(shared), private(private_list), collapse(nloop)
-!  #define YAMBO_CUDA_OR_OMP_END               !$omp end parallel do
-!#endif
+
+#if defined _OPENACC
+#  define DEV_ACC $acc
+#else
+#  define DEV_ACC !!!!
+#endif
+
+#if defined _CUDA
+#  define DEV_CUF $cuf
+#else
+#  define DEV_CUF !!!!
+#endif
+
+#if defined _OPENMP && !defined (_HAVE_DEVICE)
+#  define DEV_OMP $omp
+#else
+#  define DEV_OMP !!!!
+#endif
 

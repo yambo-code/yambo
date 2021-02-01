066f0edc55971f20c1d502dba342126b59c9e044
Davide Sangalli <davide.sangalli@gmail.com>
2021-02-01 14:29:17 +0100

    Version 5.0.0,  Revision 19573,  Hash 2ad9299d4
    MODIFIED *  config/mk/targets.mk config/version/version.m4 configure
    
    Changes:
    - [yambo_rt] Removed yambo_rt_gpl executable name
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- config/mk/targets.mk
+++ config/mk/targets.mk
@@ -6,7 +6,6 @@ ifeq ($(do_e2y),yes)
 endif
 INTERFCS = a2y c2y $(p2y) $(e2y)
 CORE     = yambo ypp $(INTERFCS)
-GPL      = yambo_rt_gpl
 UTILS    = changelog
 CLEANS   = clean_fast clean clean_all distclean
 PH_PROJ  = yambo_ph ypp_ph 
@@ -15,7 +14,7 @@ RT_PROJ  = yambo_rt ypp_rt
 NL_PROJ  = yambo_nl ypp_nl
 RTE_PROJ = yambo_rt ypp_rt
 MAIN     = $(CORE) $(PH_PROJ) $(SC_PROJ) $(RT_PROJ)  $(NL_PROJ)
-ALL      = $(CORE) $(PH_PROJ) $(SC_PROJ) $(RTE_PROJ) $(NL_PROJ) $(GPL)
-BROKEN   = yambo_phdyn
+ALL      = $(CORE) $(PH_PROJ) $(SC_PROJ) $(RTE_PROJ) $(NL_PROJ)
+BROKEN   =
 SCRIPTS  = ydb.pl
 EXE      = $(CORE) $(PH_PROJ) $(SC_PROJ) $(RTE_PROJ) $(NL_PROJ) $(BROKEN) $(SCRIPTS)

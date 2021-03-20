3eb3284170dccba10d14b039ca5fc333beb3441b
Davide Sangalli <davide.sangalli@gmail.com>
2021-03-20 13:03:40 +0100

    Version 5.0.0,  Revision 19736,  Hash bd411f0d4
    MODIFIED *  config/version/version.m4 configure include/headers/common/yambo_wfs.h modules/mod_wave_func.F wf_and_fft/WF_apply_symm.F wf_and_fft/WF_load.F
    
    Changes:
    - [yambo] Simplification of definition of WFs for gamma only case
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/headers/common/yambo_wfs.h
+++ include/headers/common/yambo_wfs.h
@@ -22,17 +22,27 @@
  MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
 */
 
+#ifdef _MOL
+#  define WF_RSPACE   real
+#else
+#  define WF_RSPACE   complex
+#endif
+
 
 #ifdef _MOL
-#  define wfconjg(x)   x 
+#  define wfconjg(x)   x
 #else
 #  define wfconjg(x)   conjg(x)
 #endif
 
-
 #ifdef _MOL
-#  define wfaimag(x)   x 
+#  define wfaimag(x)   x
 #else
 #  define wfaimag(x)   aimag(x)
 #endif
 
+#ifdef _MOL
+#  define wfcmplx(x,y)  real(x,y)
+#else
+#  define wfcmplx(x,y)  cmplx(x,y)
+#endif

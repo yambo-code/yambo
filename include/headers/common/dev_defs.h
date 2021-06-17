e1c13a2faa6d67be3229f2c6eb6c60d361ad7d0e
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2021-06-17 17:33:30 +0200

    GPU porting of el_density_and_current.F added.

--- include/headers/common/dev_defs.h
+++ include/headers/common/dev_defs.h
@@ -50,11 +50,20 @@
 #define DEV_ATTR            DEV_ATTRIBUTE
 #define DEV_PIN             DEV_PINNED
 
-!#ifdef CUDA
-!  #define YAMBO_CUDA_OR_OMP(priv_list,nloop)  !$cuf kernel do(nloop) <<<*,*>>>
-!  #define YAMBO_CUDA_OR_OMP_END
-!#else 
-!  #define YAMBO_CUDA_OR_OMP(priv_list,nloop)  !$omp parallel do default(shared), private(private_list), collapse(nloop)
-!  #define YAMBO_CUDA_OR_OMP_END               !$omp end parallel do
-!#endif
+#if defined _OPENACC
+#  define DEV_ACC $acc
+#else
+#  define DEV_ACC !!!!
+#endif
 
+#if defined _CUDA
+#  define DEV_CUF $cuf
+#else
+#  define DEV_CUF !!!!
+#endif
+
+#if defined _OPENMP && !defined (_CUDA)
+#  define DEV_OMP $omp
+#else
+#  define DEV_OMP !!!!
+#endif

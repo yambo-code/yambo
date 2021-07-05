7e5fe7cd9cc9fb1ad0f333bf59130e27cc96b3a3
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2021-07-05 12:03:15 +0200

    Version 5.0.0,  Revision 19552,  Hash 0166f2e5f
    MODIFIED *  config/version/version.m4 configure driver/options_projects.c interface/INIT_activate.F interface/INIT_load.F interface/INIT_read_command_line.F modules/mod_drivers.F nloptics/NL_initialize.F
    
    Additions:
     -Input for pump and probe added
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- src/driver/options_projects.c
+++ src/driver/options_projects.c
@@ -107,13 +107,17 @@ void options_projects(struct options_struct options[],int *i_opt)
  options[*i_opt].yambo_string="negf";
  options[*i_opt].section=desc;
  *i_opt=*i_opt+1;
+
  options[*i_opt].short_desc="Non-linear spectroscopy";
+ options[*i_opt].long_desc[0]="<string>=(n)on-linear optics, (p)ump or probe";
+ options[*i_opt].char_var=1;
  options[*i_opt].long_opt="nl";
  options[*i_opt].short_opt='u';
  options[*i_opt].bin="yambo_nl";
  options[*i_opt].yambo_string="nloptics";
  options[*i_opt].section=desc;
  *i_opt=*i_opt+1;
+
  options[*i_opt].short_desc="NEQ scattering kind";
  i_desc=0;
  options[*i_opt].long_desc[i_desc]="<string>=(ee):electron-electron interaction";

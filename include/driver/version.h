d35864121914a38d624cf95f2200a214924338a8
Davide Sangalli <davide.sangalli@gmail.com>
2021-11-12 19:01:33 +0100

    Version 5.1.0,  Revision 20564,  Hash 1f64b1a70
    MODIFIED *  include/driver/version.h bse/K_kernel.F
    
    Changes:
    - [yambo] Few changes to K_kernel imported from devel-magnons
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 20490
-#define YAMBO_HASH "aefdb48be" 
+#define YAMBO_REVISION 20564
+#define YAMBO_HASH "1f64b1a70" 
 

a1c5f31d5c935fa92434c49f1f48eb6344766669
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2021-11-18 10:50:51 +0100

    Version 5.1.0,  Revision 21107,  Hash d4d33af6c
    MODIFIED *  include/driver/version.h collisions/COLLISIONS_load.F
    
    Bugs:
     -Fixed small typos
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21106
-#define YAMBO_HASH "f0ee93bb9" 
+#define YAMBO_REVISION 21107
+#define YAMBO_HASH "d4d33af6c" 
 

d560822a225e1b26150e1ea950bc28eba5cf7db8
Davide Sangalli <davide.sangalli@gmail.com>
2022-01-19 09:32:36 +0100

    Automatic commit: configure regenerated after merge

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21128
-#define YAMBO_HASH "c8bb0e2bd" 
+#define YAMBO_REVISION 21160
+#define YAMBO_HASH "0e7e30eea" 
 

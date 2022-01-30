51d28f79e34583cb56ea237ba6a28c39a4b40735
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-01-30 17:39:11 +0100

    Version 5.1.0,  Revision 21145,  Hash edb5061b4
    MODIFIED *  include/driver/version.h el-ph/EXCPH_gkkp_driver.F el-ph/EXCPH_gkkp_eval.F
    
    Additions:
     -It works.... checks in progress
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21144
-#define YAMBO_HASH "ae4cc26ee" 
+#define YAMBO_REVISION 21145
+#define YAMBO_HASH "db2a6ab82" 
 

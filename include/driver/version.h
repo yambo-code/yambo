17b628a410dfdff19b9315eedaf46c4c1792c875
Davide Sangalli <davide.sangalli@gmail.com>
2022-02-11 13:14:46 +0100

    Version 5.1.1,  Revision 21326,  Hash eea6d3ebc
    MODIFIED *  include/driver/version.h lib/yambo driver/options_control.c driver/options_interfaces.c driver/options_projects.c driver/options_yambo.c driver/options_ypp.c pol_function/X_AVERAGE_do_it.F
    
    Changes:
    - [yambo] Fixed compilation
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 1
-#define YAMBO_REVISION 21325
-#define YAMBO_HASH "58c5ac9cc" 
+#define YAMBO_REVISION 21326
+#define YAMBO_HASH "eea6d3ebc" 
 

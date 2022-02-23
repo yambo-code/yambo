1e40394c9edf8ab73832b2b2221d398b0f160404
Ignacio Martin Alliati <ialliati01@qub.ac.uk>
2022-02-23 20:06:37 +0000

    Version 5.1.0,  Revision 20418,  Hash fb5ebd2df
    MODIFIED *  include/driver/version.h floquet/FL_determinants.F floquet/FL_driver.F floquet/FL_overlaps.F floquet/FL_polarization.F modules/mod_fl_optics.F
    
    Bugs:
    -
    
    Additions:Info on SC cycle printed out in report file.
    new variable n_tot_time_steps, which can now be different from
    n_tot_fl_modes
    -
    
    Changes:
    -
    
    Patch sent by:  Ignacio Martin Alliati <ialliati01@qub.ac.uk>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 20417
-#define YAMBO_HASH "ca4a2b9f6"
+#define YAMBO_REVISION 20418
+#define YAMBO_HASH "fb5ebd2df"
 

61ffb3c88078326ed73ec999c26c82c5c3b69a68
Davide Sangalli <davide.sangalli@gmail.com>
2022-04-27 17:37:51 +0200

    Version 5.1.0,  Revision 21415,  Hash e7bbccebf
    MODIFIED *  include/driver/version.h exc-ph/EXCPH_gkkp_driver.F
    
    Bugs:
    - [yambo_ph] Small fix
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21413
-#define YAMBO_HASH "f8f0ab248" 
+#define YAMBO_REVISION 21415
+#define YAMBO_HASH "e7bbccebf" 
 

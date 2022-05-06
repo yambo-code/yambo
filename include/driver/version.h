0caff5775cdc8ba38a228d179f280ad34d6a3f8d
Andrea Marini <andrea.marini@cnr.it>
2022-05-06 13:55:38 +0200

    Version 5.1.1,  Revision 21384,  Hash 0a25f6c8c
    MODIFIED *  include/driver/version.h
    
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 1
-#define YAMBO_REVISION 21383
-#define YAMBO_HASH "ac1ae7f4d" 
+#define YAMBO_REVISION 21386
+#define YAMBO_HASH "0a25f6c8c" 
 

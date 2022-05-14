a365006c3b6d129c654229a3ef0fa4ed3acc52a2
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-05-14 16:53:32 +0200

    Version 5.1.0,  Revision 21422,  Hash 5b51fcea8
    MODIFIED *  include/driver/version.h el-ph/Eval_Efermi_DbGd.F
    
    Additions:
     -Fixed approximated calculation of the Fermi Energy
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21421
-#define YAMBO_HASH "fa8c334bd" 
+#define YAMBO_REVISION 21422
+#define YAMBO_HASH "5b51fcea8" 
 

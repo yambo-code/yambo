e9f8122bf69a423675e3cb65404d02782e857046
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-06-03 16:59:02 +0200

    Version 5.1.0,  Revision 21763,  Hash 1edac2efc
    MODIFIED *  include/driver/version.h exc-ph/EXCPH_eval_sat.F exc-ph/EXCPH_optics.F modules/mod_EXCPH.F
    
    Additions:
     -Added the possibility of calculations without matrix elements
     -Code cleaning
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21763
-#define YAMBO_HASH "f88b112f3" 
+#define YAMBO_REVISION 21764
+#define YAMBO_HASH "1edac2efc" 
 

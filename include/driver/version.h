bbe289ee60a5232c895d78f09e6489d4dd831675
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-06-09 16:47:38 +0200

    Version 5.1.0,  Revision 21795,  Hash b5f9df9cb
    MODIFIED *  include/driver/version.h exc-ph/Satellite_DbGrid.F
    
    Changes:
    -Better check to the weight in the DB-grid but not change in the result
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21794
-#define YAMBO_HASH "e28a0a09c" 
+#define YAMBO_REVISION 21795
+#define YAMBO_HASH "b5f9df9cb" 
 

ad4222a52cc6a4f2beed7f9e190c0d8cc1290a0c
Davide Sangalli <davide.sangalli@gmail.com>
2022-06-30 11:07:51 +0200

    Version 5.1.0,  Revision 21633,  Hash 1c165dc0c
    MODIFIED *  include/driver/version.h bse/K.F bse/K_driver_init.F
    
    Bugs:
    - [yambo] Fixed compilation after merge
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21606
-#define YAMBO_HASH "fdc4cd56d" 
+#define YAMBO_REVISION 21633
+#define YAMBO_HASH "1c165dc0c" 
 

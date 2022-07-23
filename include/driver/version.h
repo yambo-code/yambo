fde6e2a070b48329729fa49b51c07f211ed11235
Davide Sangalli <davide.sangalli@gmail.com>
2022-07-23 22:22:53 +0200

    Version 5.1.0,  Revision 21421,  Hash cb7ae81ec
    MODIFIED *  include/driver/version.h bse/K.F
    
    Bugs:
    - [yambo] Fix for BSE in full mode with coupling imported from bug-fixes
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21419
-#define YAMBO_HASH "a5ad0d553" 
+#define YAMBO_REVISION 21421
+#define YAMBO_HASH "cb7ae81ec" 
 

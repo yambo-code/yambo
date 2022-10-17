2e2babd89ad10a6400363781b73ff8b0cbbf3942
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-10-17 15:59:02 +0200

    Version 5.1.0,  Revision 22008,  Hash e7a2e5c5d
    MODIFIED *  include/driver/version.h exc-ph/EXCPH_optics.F modules/mod_vec_operate.F ypp/excitons/excitons_bands.F
    
    Additions:
     -2D version of bands distortion
    
    Changes:
    -
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 22008
-#define YAMBO_HASH "c324aaaf0" 
+#define YAMBO_REVISION 22009
+#define YAMBO_HASH "e7a2e5c5d" 
 

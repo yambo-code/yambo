c5ced23c56d6dbd48f6b42649dfcc4bb70c1f586
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2022-11-28 11:03:28 +0100

    Version 5.1.0,  Revision 22026,  Hash f8e1a49d1
    MODIFIED *  driver/ypp.F include/driver/version.h ypp/el-ph/.objects ypp/interface/INIT_load_ypp.F ypp/interface/INIT_ypp.F ypp/modules/mod_YPP_ELPH.F
    
    NEW *  ypp/el-ph/ELPH_sngl_db.F
    
    Bugs:
    -
    
    Additions:
     -New file to read a single database without symmetries
    
    Changes:
    -
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 22025
-#define YAMBO_HASH "fdbe1c1e5" 
+#define YAMBO_REVISION 22026
+#define YAMBO_HASH "f8e1a49d1" 
 

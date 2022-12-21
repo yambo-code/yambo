dd5176fa7986e31643f98ff11fcbedcb2d3cba3e
Davide Sangalli <davide.sangalli@gmail.com>
2022-12-22 00:29:48 +0100

    Version 5.1.0,  Revision 22138,  Hash 9deecc59b
    MODIFIED *  include/driver/version.h a2y/a2y_gkkp.F
    
    Changes:
    - [a2y] inverted gppk indexes n and m
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21777
-#define YAMBO_HASH "ef4f442a1" 
+#define YAMBO_REVISION 22138
+#define YAMBO_HASH "9deecc59b" 
 

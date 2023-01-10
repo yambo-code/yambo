c02e7e1109e7dfd2b8cd821f5fa0a8921811bb76
Davide Sangalli <davide.sangalli@gmail.com>
2023-01-10 03:20:18 +0100

    Version 5.1.0,  Revision 21787,  Hash 8dfdd9c16
    MODIFIED *  include/driver/version.h sbin/compilation/check_updated_locks.sh
    
    NEW *
    
    Changes:
    - [compilation] Naming of save folders simplified
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- sbin/compilation/check_updated_locks.sh
+++ sbin/compilation/check_updated_locks.sh
@@ -28,13 +28,15 @@ sorted_locks=$(echo "$lock_files"|tr " " "\n"|sort|uniq|tr "\n" " ")
 # Locks -> string
 #
 lock_string=""
-save_dir=""
+save_dir="objects"
 for lock in $sorted_locks
 do
  lock=`echo $lock | sed "s/.lock//"`
  lock=`basename $lock`
  lock_string="${lock} ${lock_string}"
- save_dir="${lock}_${save_dir}"
+ if test -f "$dir/${lock}_project.dep"; then
+   save_dir="${lock}_${save_dir}"
+ fi
 done
 if [[ -z $save_dir ]] ; then
  return
@@ -44,12 +46,14 @@ save_dir="${save_dir}.save"
 # -D* -> string
 #
 flag_string=""
-restore_dir=""
+restore_dir="objects"
 for flag in $sorted_precomps
 do
  flag=`echo $flag | sed "s/\-D_//"`
  flag_string="${flag} ${flag_string}"
- restore_dir="${flag}_${restore_dir}"
+ if test -f "$dir/${flag}_project.dep"; then
+   restore_dir="${flag}_${restore_dir}"
+ fi
 done
 restore_dir="${restore_dir}.save"
 #

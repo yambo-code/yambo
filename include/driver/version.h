53df35f2c6486d7b6d5af9d698e90ed67fc29514
Andrea Marini <andrea.marini@cnr.it>
2023-02-03 08:25:35 +0100

    Version 5.1.0,  Revision 21800,  Hash 7fe3130aa
    MODIFIED *  config/version/version.m4 include/driver/version.h bse/K_observables.F io_serial/io_COLLISIONS.F
    
    NEW *  bse/SC_project.dep driver/SC_project.dep linear_algebra/SC_project.dep matrices/SC_project.dep real_time_drivers/ELPH_project.dep
    
    Additions:
    - Added missing (?) dep files.
    
    Changes:
    - Minor fixes.
    
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 21874
-#define YAMBO_HASH "f6ee9dd3c" 
+#define YAMBO_REVISION 21876
+#define YAMBO_HASH "7fe3130aa" 
 

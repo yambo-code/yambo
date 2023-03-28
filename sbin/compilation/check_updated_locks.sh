8e920cb4bac45f423230bed9a3833932e4f6a65f
Andrea Marini <andrea.marini@cnr.it>
2023-03-28 17:35:00 +0200

    Version 5.1.0,  Revision 22019,  Hash 843c41778
    MODIFIED *  config/version/version.m4 include/driver/version.h sbin/compilation/check_updated_locks.sh sbin/compilation/fix_locks.sh sbin/compilation/object_save_and_restore.sh hamiltonian/RT_project.dep hamiltonian/SC_project.dep hamiltonian/V_qp_basis_to_H.F modules/.objects qp/DOUBLE_project.dep qp/QED_project.dep qp/SC_project.dep real_time_control/RT_alloc.F real_time_control/RT_free.F real_time_initialize/RT_occupations_and_levels_init.F
    
    Bugs:
    - Compilation (project dependences) bugs fixed
    
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- sbin/compilation/check_updated_locks.sh
+++ sbin/compilation/check_updated_locks.sh
@@ -33,14 +33,11 @@ for lock in $sorted_locks
 do
  lock=`echo $lock | sed "s/.lock//"`
  lock=`basename $lock`
- lock_string="${lock} ${lock_string}"
- if test -f "$dir/${lock}_project.dep"; then
-   save_dir="${lock}_${save_dir}"
+ if [[ -f  "$dir/${lock}_project.dep" ]]; then
+  lock_string="${lock} ${lock_string}"
+  save_dir="${lock}_${save_dir}"
  fi
 done
-if [[ -z $lock_string ]] ; then
- return
-fi
 save_dir="${save_dir}.save"
 #
 # -D* -> string
@@ -50,9 +47,9 @@ restore_dir="objects"
 for flag in $sorted_precomps
 do
  flag=`echo $flag | sed "s/\-D_//"`
- flag_string="${flag} ${flag_string}"
- if test -f "$dir/${flag}_project.dep"; then
-   restore_dir="${flag}_${restore_dir}"
+ if [[ -f  "$dir/${flag}_project.dep" ]]; then
+  flag_string="${flag} ${flag_string}"
+  restore_dir="${flag}_${restore_dir}"
  fi
 done
 restore_dir="${restore_dir}.save"
@@ -67,22 +64,24 @@ unmatched="$missing $new"
 # Now new nor missing -> exit
 #
 if [[ -z $new ]] && [[ -z $missing ]]; then
+ source ./sbin/compilation/verbosity.sh "check_updated_locks.sh: No new or missing projects"
  return
 fi
 #
-if [ "$VERB" == 1 ] ; then
- echo "D" $dir
- echo "L" $lock_string
- echo "F" $flag_string
- echo "M" $missing
- echo "N" $new
- echo "SAVE" $save_dir
- echo "RESTORE" $restore_dir
-fi
+# Ydriver is to be recompiled anyway as the library name depends on the executable and cannot be saved
+if [[ "$target" == *"_Ydriver_"* ]] ; then DIR_is_to_recompile=1; fi
 #
+source ./sbin/compilation/verbosity.sh "locks"
+#
+# Save & Restore
 source ./sbin/compilation/object_save_and_restore.sh
-if [ "$FOLDER_OK" == 1 ] ; then
- return ;
+#
+if [ "$DIR_saved" == "yes" ] ; then
+ source ./sbin/compilation/verbosity.sh "check_updated_locks.sh: $dir has been saved"
+fi
+if [ "$DIR_restored" == "yes" ] ; then
+ source ./sbin/compilation/verbosity.sh "check_updated_locks.sh: $dir has been restored"
+ return
 fi
 #
 # tag new objects to be compiled
@@ -91,21 +90,16 @@ for lock in $unmatched
 do
  #
  if test -f "$dir/${lock}_project.dep"; then
-  deps=`cat $dir/${lock}_project.dep` ;
-  refs=$deps ;
-  if [ -f $dir/$restore_dir/files.dep ] ; then refs=`cat $dir/$restore_dir/files.dep` ; fi
-  for file in $deps; do
-   if [[ "$refs" == *"$file"* ]]; then
-    if [ "$VERB" == 1 ] ; then echo "$step preparing $file"; fi
-    DIR_is_to_recompile=1
-    if [ "$lock" == "DOUBLE" ]; then
-      obj=$file
-      source ./sbin/compilation/object_remove.sh "remove" "locks"
-      continue;
-    fi
-    obj=$file
-    source ./sbin/compilation/check_object_childs.sh "locks"
+  deps=`cat $dir/${lock}_project.dep`
+  for dep_file in $deps; do
+   source ./sbin/compilation/verbosity.sh "check_updated_locks.sh: $dep_file must be recompiled"
+   source ./sbin/compilation/name_me.sh $dir/$dep_file "no_search"
+   DIR_is_to_recompile=1
+   if [ "$lock" == "DOUBLE" ]; then
+    source ./sbin/compilation/object_remove.sh "remove" "locks"
+    continue;
    fi
+   source ./sbin/compilation/check_object_childs.sh "locks"
   done
  fi
  #

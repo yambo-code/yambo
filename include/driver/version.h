0692d594de4005bce629c9204674712bb4dc3b87
Davide Sangalli <davide.sangalli@gmail.com>
2023-04-14 00:45:30 +0200

    Version 5.1.0,  Revision 22079,  Hash edec75dbd
    MODIFIED *  include/driver/version.h pol_function/OPTICS_driver.F
    
    Bugs:
    - [yambo] Fixed definition of Alpha RPA when eels is not evaluated
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- include/driver/version.h
+++ include/driver/version.h
@@ -25,6 +25,6 @@
 #define YAMBO_VERSION 5
 #define YAMBO_SUBVERSION 1
 #define YAMBO_PATCHLEVEL 0
-#define YAMBO_REVISION 22078
-#define YAMBO_HASH "8a773ee5a" 
+#define YAMBO_REVISION 22079
+#define YAMBO_HASH "edec75dbd" 
 

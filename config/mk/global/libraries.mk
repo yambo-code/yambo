9ab27815e71948df2b613886c1161b92d0699c14
Davide Sangalli <davide.sangalli@gmail.com>
2023-04-27 18:15:47 +0200

    Version 5.1.0,  Revision 22138,  Hash d46c918bb
    MODIFIED *  config/mk/global/libraries.mk config/report.in config/setup.in lib/archive/Makefile.loc lib/archive/package.list lib/petsc/Makefile.loc interface/DOUBLE_project.dep interface/RT_project.dep interface/SC_project.dep
    
    Changes:
    - Few things restored to develop version:
    a) Fixed linking of lapack and blas libraries in slepc and connected report
    b) collisions folder is compiled only by _RT and _NL project, no need to compile it in yambo
    c) Ydriver does not need to be re-downloaded every time
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- config/mk/global/libraries.mk
+++ config/mk/global/libraries.mk
@@ -16,18 +16,18 @@ YLIBIO_LD     = $(YLIBIO)
 # Source code
 #
 BASIC_LIBS   = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing Yio io $(IO_MODE) \
-               xc_functionals interface stop_and_restart wf_and_fft bz_ops coulomb 
+               xc_functionals interface stop_and_restart wf_and_fft bz_ops coulomb
 BASIC_LIBS_LD= tools memory allocations communicate modules matrices linear_algebra bz_ops parallel parser output common timing Yio io $(IO_MODE) \
-               xc_functionals interface stop_and_restart wf_and_fft coulomb 
+               xc_functionals interface stop_and_restart wf_and_fft coulomb
 
-MAIN_LIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function collisions qp acfdt bse
-MAIN_LIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function collisions qp acfdt bse
+MAIN_LIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function qp acfdt bse
+MAIN_LIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function qp acfdt bse
 
-PJ_PHLIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function collisions el-ph qp acfdt bse real_time_initialize ph-el
-PJ_PHLIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function collisions el-ph qp acfdt bse real_time_initialize ph-el
+PJ_PHLIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function el-ph qp acfdt bse real_time_initialize ph-el
+PJ_PHLIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function el-ph qp acfdt bse real_time_initialize ph-el
 
-PJ_SCLIBS    = $(MAIN_LIBS) hamiltonian sc
-PJ_SCLIBS_LD = $(MAIN_LIBS_LD) hamiltonian sc
+PJ_SCLIBS    = $(MAIN_LIBS) collisions hamiltonian sc
+PJ_SCLIBS_LD = $(MAIN_LIBS_LD) hamiltonian collisions sc
 
 PJ_RT_GPL_LIBS   = $(BASIC_LIBS) interpolate real_time_control qp_control setup \
                    tddft dipoles pol_function qp acfdt bse collisions hamiltonian \
@@ -37,20 +37,20 @@ PJ_RT_GPL_LIBS_LD= $(BASIC_LIBS_LD) interpolate real_time_control qp_control set
                    real_time_hamiltonian real_time_propagation real_time_initialize real_time_drivers
 
 PJ_RTITLIBS    = $(BASIC_LIBS) interpolate qp_control setup \
-               tddft dipoles pol_function collisions el-ph qp acfdt bse hamiltonian \
+               tddft dipoles pol_function el-ph qp acfdt bse collisions hamiltonian \
                real_time_control real_time_hamiltonian real_time_propagation real_time_lifetimes real_time_iterative_el-ph \
                real_time_initialize ph-el real_time_drivers
 PJ_RTITLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup \
-               tddft dipoles pol_function collisions el-ph qp acfdt bse hamiltonian \
+               tddft dipoles pol_function el-ph qp acfdt bse hamiltonian collisions \
                real_time_hamiltonian real_time_propagation real_time_lifetimes real_time_iterative_el-ph \
                real_time_initialize ph-el real_time_drivers
 
 PJ_RTLIBS    = $(BASIC_LIBS) interpolate qp_control setup \
-               tddft dipoles pol_function collisions el-ph qp acfdt bse hamiltonian \
+               tddft dipoles pol_function el-ph qp acfdt bse collisions hamiltonian \
                real_time_control real_time_hamiltonian real_time_propagation real_time_lifetimes \
                real_time_initialize ph-el real_time_drivers
 PJ_RTLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup \
-               tddft dipoles pol_function collisions el-ph qp acfdt bse hamiltonian \
+               tddft dipoles pol_function el-ph qp acfdt bse hamiltonian collisions \
                real_time_hamiltonian real_time_propagation real_time_lifetimes \
                real_time_initialize ph-el real_time_drivers
 
@@ -65,28 +65,27 @@ PJ_NLLIBS_LD = $(PJ_RTLIBS_LD) nloptics
                setup interface stop_and_restart bz_ops 
 #
 # YPP
-#=====
+#
 YPP_BASIC_LIBS     = modules interface qp plotting k-points symmetries bits electrons dipoles
+YPP_BASIC_LIBS_LD  = modules interface qp plotting k-points symmetries bits electrons dipoles 
 YPP_LIBS           = $(YPP_BASIC_LIBS) excitons
+YPP_LIBS_LD        = $(YPP_BASIC_LIBS_LD) excitons
 YPPPH_LIBS         = $(YPP_BASIC_LIBS) el-ph excitons
+YPPPH_LIBS_LD      = $(YPP_BASIC_LIBS_LD) el-ph excitons
 YPPRT_LIBS         = $(YPP_BASIC_LIBS) el-ph real_time excitons
-# .. LD
-YPP_BASIC_LIBS_LD  = $(YPP_BASIC_LIBS)
-YPP_LIBS_LD        = $(YPP_LIBS) 
-YPPPH_LIBS_LD      = $(YPPPH_LIBS)
-YPPRT_LIBS_LD      = $(YPPRT_LIBS)
+YPPRT_LIBS_LD      = $(YPP_BASIC_LIBS_LD) el-ph real_time excitons
 #
 # YAMBO sources needed by YPP
-#=============================
-YPP_MAIN_LIBS      = $(BASIC_LIBS) interpolate qp_control setup interface tddft dipoles pol_function collisions qp bse
-YPP_MAIN_LIBS_LD   = $(BASIC_LIBS_LD) interpolate qp_control setup interface tddft dipoles pol_function collisions qp bse
-YPPSC_MAIN_LIBS    = $(YPP_MAIN_LIBS) hamiltonian sc
-YPPSC_MAIN_LIBS_LD = $(YPP_MAIN_LIBS_LD) hamiltonian sc
+#
+YPP_MAIN_LIBS      = $(BASIC_LIBS) interpolate qp_control setup interface tddft dipoles pol_function qp bse
+YPP_MAIN_LIBS_LD   = $(BASIC_LIBS_LD) interpolate qp_control setup interface tddft dipoles pol_function qp bse
+YPPSC_MAIN_LIBS    = $(YPP_MAIN_LIBS) collisions hamiltonian sc
+YPPSC_MAIN_LIBS_LD = $(YPP_MAIN_LIBS_LD) collisions hamiltonian sc
 YPPRT_MAIN_LIBS    = $(BASIC_LIBS) real_time_control interpolate qp_control setup interface \
-                     dipoles pol_function collisions el-ph qp bse hamiltonian 
+                     dipoles pol_function el-ph qp bse collisions hamiltonian 
 YPPRT_MAIN_LIBS_LD = $(BASIC_LIBS_LD) real_time_control interpolate qp_control setup interface \
-                     dipoles pol_function collisions el-ph qp bse hamiltonian 
+                     dipoles pol_function el-ph qp bse hamiltonian collisions
 YPPNL_MAIN_LIBS    = $(BASIC_LIBS) real_time_control interpolate qp_control setup interface \
-                     dipoles pol_function collisions el-ph qp bse hamiltonian nloptics 
+                     dipoles pol_function el-ph qp bse collisions hamiltonian nloptics 
 YPPNL_MAIN_LIBS_LD = $(BASIC_LIBS_LD) real_time_control interpolate qp_control setup interface \
-                     dipoles pol_function collisions el-ph qp bse hamiltonian nloptics 
+                     dipoles pol_function el-ph qp bse hamiltonian collisions nloptics 

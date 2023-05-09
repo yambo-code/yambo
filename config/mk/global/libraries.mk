c66023310226d28975fd2c76dee813da4c9f89d0
Davide Sangalli <davide.sangalli@gmail.com>
2023-05-09 13:59:14 +0200

    Version 5.1.0,  Revision 22166,  Hash ab8307bf7
    MODIFIED *  config/mk/global/libraries.mk include/version/version.m4
    
    Bugs:
    - [compilation] Restored RTLIBS accidentally removed in previous commit
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- config/mk/global/libraries.mk
+++ config/mk/global/libraries.mk
@@ -45,6 +45,16 @@ PJ_RTITLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup
                real_time_hamiltonian real_time_propagation real_time_lifetimes real_time_iterative_el-ph \
                real_time_initialize ph-el real_time_drivers
 
+PJ_RTLIBS    = $(BASIC_LIBS) interpolate qp_control setup \
+               tddft dipoles pol_function el-ph qp acfdt bse collisions hamiltonian \
+               real_time_control real_time_hamiltonian real_time_propagation real_time_lifetimes \
+               real_time_initialize ph-el real_time_drivers
+PJ_RTLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup \
+               tddft dipoles pol_function el-ph qp acfdt bse hamiltonian collisions \
+               real_time_hamiltonian real_time_propagation real_time_lifetimes \
+               real_time_initialize ph-el real_time_drivers
+
+
 PJ_NLLIBS    = $(BASIC_LIBS) interpolate qp_control setup \
                tddft dipoles pol_function qp acfdt bse collisions hamiltonian \
                real_time_control real_time_hamiltonian real_time_propagation \

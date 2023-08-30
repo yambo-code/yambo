541ffff76064a4e56fca37791a426596fa396599
Andrea Marini <andrea.marini@cnr.it>
2023-08-30 13:45:04 +0200

    Version 5.1.0,  Revision 22264,  Hash 89abd914e5
    MODIFIED *  config/mk/defs.mk.in config/mk/global/defs.mk.in config/mk/local/defs.mk.in include/version/version.m4 sbin/developer_tools/file_splitter.pl sbin/developer_tools/msg_unit_shift.pl sbin/make_message.pl
    
    Changes:
    - Copyright updated: .in and .pl files
    
    Patch sent by:  Andrea Marini <andrea.marini@cnr.it>

--- sbin/make_message.pl
+++ sbin/make_message.pl
@@ -1,32 +1,17 @@
 #!/usr/bin/perl -w
 #
-#        Copyright (C) 2000-2022 the YAMBO team
-#              http://www.yambo-code.org
+# License-Identifier: GPL
 #
-# Authors (see AUTHORS file for details): CH
+# Copyright (C) 2000 The Yambo Team
 #
-# This file is distributed under the terms of the GNU
-# General Public License. You can redistribute it and/or
-# modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation;
-# either version 2, or (at your option) any later version.
+# Authors (see AUTHORS file for details): CDH
 #
-# This program is distributed in the hope that it will
-# be useful, but WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A
-# PARTICULAR PURPOSE.  See the GNU General Public License
-# for more details.
+# Wonderful script to generate a commit message template
 #
-# You should have received a copy of the GNU General Public
-# License along with this program; if not, write to the Free
-# Software Foundation, Inc., 59 Temple Place - Suite 330,Boston,
-# MA 02111-1307, USA or visit http://www.gnu.org/copyleft/gpl.txt.
-#
-# Wonderful script to generate a commit message template (CDH)
 # Usage: In the YAMBO root directory
 #
-# sbin/make_message.pl    or
-# sbin/make_message.pl -p="Myrta"   if a patch was supplied by e.g. Myrta
+#  sbin/make_message.pl    or
+#  sbin/make_message.pl -p="Myrta"   if a patch was supplied by e.g. Myrta
 #
 use Getopt::Long;
 &GetOptions("p=s"          => \$patchname);

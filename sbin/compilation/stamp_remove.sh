eb83e72f83ffd4647967ca1d1d01c78bb345f038
Davide Sangalli <davide.sangalli@gmail.com>
2023-10-30 11:36:31 +0100

    Version 5.2.0,  Revision 22740,  Hash aa41f561c5
    MODIFIED *  include/version/version.m4 sbin/compilation/stamp_remove.sh
    
    Bugs:
    - Issue https://github.com/yambo-code/yambo/issues/61
      Small refinement of the fix
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- sbin/compilation/stamp_remove.sh
+++ sbin/compilation/stamp_remove.sh
@@ -4,8 +4,9 @@
 #
 # Copyright (C) 2018 The Yambo Team
 #
-# Authors (see AUTHORS file for details): AM
+# Authors (see AUTHORS file for details): AM DS
 #
+stamp="none"
 if [ $1 == "goal" ] && [ ! -z $goal ] ; then stamp=$compdir/config/stamps_and_lists/${goal}.stamp ; fi
 if [ $1 == "target.a" ] && [ ! -z $target ] ; then stamp=$compdir/config/stamps_and_lists/${target}.a.stamp ; fi
 if [ $1 == "lib" ] && [ ! -z $llib ] ; then stamp=$compdir/config/stamps_and_lists/lib${llib}.a.stamp; fi

4e7a3776a5fc6fd28cd7f86dbd99ce7ad0d1623a
Davide Sangalli <davide.sangalli@gmail.com>
2023-10-30 14:39:59 +0100

    Version 5.2.0,  Revision 22741,  Hash eb83e72f83
    MODIFIED *  include/version/version.m4 sbin/compilation/stamp_remove.sh
    
    Bugs:
    - https://github.com/yambo-code/yambo/issues/61
      Previous fix contained a \ in place of a /
      Hopefully this is the correct fix
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- sbin/compilation/stamp_remove.sh
+++ sbin/compilation/stamp_remove.sh
@@ -9,8 +9,13 @@
 stamp="none"
 if [ $1 == "goal" ] && [ ! -z $goal ] ; then stamp=$compdir/config/stamps_and_lists/${goal}.stamp ; fi
 if [ $1 == "target.a" ] && [ ! -z $target ] ; then stamp=$compdir/config/stamps_and_lists/${target}.a.stamp ; fi
-if [ $1 == "lib" ] && [ ! -z $llib ] ; then stamp=$compdir/config/stamps_and_lists/lib${llib}.a.stamp; fi
-if [ $1 == "lib" ] && [ ! -z $llib ] && [[ "$file_src" == *"\ypp"* ]]  ; then stamp=$compdir/config/stamps_and_lists/lib_ypp_${llib}.a.stamp; fi
+if [ $1 == "lib" ] && [ ! -z $llib ] ; then
+ if [[ "$file_src" == *"/ypp/$llib"* ]]  ; then
+  stamp=$compdir/config/stamps_and_lists/lib_ypp_${llib}.a.stamp;
+ else
+  stamp=$compdir/config/stamps_and_lists/lib${llib}.a.stamp;
+ fi
+fi
 if [ $1 == "exe" ] ; then stamp=`find $compdir/config/stamps_and_lists/ -name "compiling*"  | sed "s/compiling_//"`; fi
 source ./sbin/compilation/verbosity.sh "stamp_remove.sh (mode $1) running on $dir: remove $stamp"
 rm -f $stamp

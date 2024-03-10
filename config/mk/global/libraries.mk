58283a0ff9093aa84765b664337dfbdaf27dda28
Davide Sangalli <davide.sangalli@gmail.com>
2024-03-10 11:38:05 +0100

    Version 5.2.0,  Revision 23481,  Hash 96759d2753
    MODIFIED *  config/mk/global/libraries.mk driver/yambo.F include/version/version.m4 bse/.objects bse/DOUBLE_project.dep bz_ops/.objects bz_ops/DOUBLE_project.dep bz_ops/k_build_up_BZ_tables.F bz_ops/k_expand.F exc-ph/EXCPH_gkkp_driver.F exc-ph/EXCPH_gkkp_eval_ber.F exc-ph/EXCPH_gkkp_eval_pal.F io/DOUBLE_project.dep modules/mod_BS_solvers.F modules/mod_EXCPH.F modules/mod_R_lattice.F modules/mod_electrons.F modules/mod_interfaces.F modules/mod_wave_func.F tools/DOUBLE_project.dep wf_and_fft/.objects wf_and_fft/DOUBLE_project.dep ypp/symmetries/k_build_map.F
    
    NEW *  bse/BSE_utilities.F bz_ops/k_small_group.F io/io_WF_phases.F wf_and_fft/EXC_WF_symm_qpoint.F wf_and_fft/WF_phase_matrices.F wf_and_fft/WF_symm_kpoint_g.F
    
    Changes:
    - WF_phases imported from branch phys-excitons
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- config/mk/global/libraries.mk
+++ config/mk/global/libraries.mk
@@ -27,14 +27,14 @@ YLIBIO_LD     = $(YLIBIO)
 #
 BASIC_LIBS   = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing Yio io $(IO_MODE) \
                xc_functionals interface stop_and_restart wf_and_fft bz_ops coulomb
-BASIC_LIBS_LD= tools memory allocations communicate modules matrices linear_algebra bz_ops parallel parser output common timing Yio io $(IO_MODE) \
+BASIC_LIBS_LD= tools memory allocations communicate modules matrices linear_algebra common bz_ops parallel parser output timing Yio io $(IO_MODE) \
                xc_functionals interface stop_and_restart wf_and_fft coulomb
 
 MAIN_LIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function qp acfdt bse
 MAIN_LIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function qp acfdt bse
 
 PJ_PHLIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function el-ph exc-ph qp acfdt bse
-PJ_PHLIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function el-ph exc-ph qp acfdt bse
+PJ_PHLIBS_LD = $(BASIC_LIBS_LD) interpolate qp_control setup tddft dipoles pol_function el-ph bse exc-ph qp acfdt
 
 PJ_SCLIBS    = $(MAIN_LIBS) collisions hamiltonian sc
 PJ_SCLIBS_LD = $(MAIN_LIBS_LD) hamiltonian collisions sc

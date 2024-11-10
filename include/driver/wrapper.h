7db510283b355ba8606eb7d5b18bb7d3d5a23bed
Davide Sangalli <davide.sangalli@gmail.com>
2024-11-10 23:47:14 +0100

    Addressing issued #150
    Version 5.3.0,  Revision 23799,  Hash d803014f5f
    
    MODIFIED *  config/configure.ac config/mk/global/actions/compile_interfaces.mk config/mk/global/actions/compile_yambo.mk config/mk/global/actions/compile_yambo_libraries.mk config/mk/global/actions/compile_ypp.mk config/mk/global/actions/dependencies.mk config/mk/global/actions/download_external_libraries.mk config/mk/global/actions/help.mk config/mk/global/functions/cleaning.mk config/mk/global/functions/help.mk config/mk/global/functions/mk_exe.mk config/mk/global/libraries.mk config/mk/local/functions.mk config/mk/local/makefile config/report.in config/setup.in configure include/version/version.m4 lib/archive/Makefile.loc lib/archive/git.list.in lib/archive/package.list sbin/compilation/check_updated_locks.sh sbin/compilation/dependencies_project.sh sbin/compilation/object_save_and_restore.sh Ymodules/DOUBLE_project.dep common/DOUBLE_project.dep driver/.objects driver/DOUBLE_project.dep driver/QED_project.dep driver/YPP_RT_project.dep driver/a2y_project.dep linear_algebra/DOUBLE_project.dep qp/SC_project.dep wf_and_fft/DOUBLE_project.dep
    
    NEW *  include/driver/driver.h include/driver/fortran_arguments.h include/driver/kind.h include/driver/wrapper.h driver/C_driver_transfer.F driver/YPP_ELPH_project.dep driver/YPP_NL_project.dep driver/YPP_SC_project.dep driver/c2y_project.dep driver/command_line.c driver/driver.c driver/get_libraries.c driver/get_runlevel.c driver/get_running_project.c driver/get_running_tool.c driver/get_version.c driver/input_file.c driver/launcher.c driver/load_environments.c driver/mod_C_driver.F driver/options_help.c driver/options_maker.c driver/p2y_project.dep driver/title.c driver/tool_init.c driver/usage.c driver/use_me.c driver/winsize.c driver/yambo_project.dep driver/ypp_project.dep gpu_test/DOUBLE_project.dep gpu_test_mod/DOUBLE_project.dep
    
    DELETED *  config/m4/yambo_driver.m4 include/driver/fortran_ydriver.h
    
    Bugs:
    -
    
    Additions:
    -
    
    Changes:
    -
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

---
+++ include/driver/wrapper.h
@@ -0,0 +1,23 @@
+/*
+  License-Identifier: GPL
+ 
+  Copyright (C) 2020 The Yambo Team
+ 
+  Authors (see AUTHORS file for details): DS
+*/
+/*
+ C wrapper
+*/ 
+#if defined _C_US
+ #define C_FUNC(name,NAME) name ## _
+#else
+ #define C_FUNC(name,NAME) name
+#endif
+/*
+ F90 wrapper
+*/
+#if defined _FORTRAN_US
+ #define F90_FUNC(name) name ## _
+#else
+ #define F90_FUNC(name) name
+#endif
---
+++ src/driver/get_libraries.c
@@ -0,0 +1,87 @@
+/*
+  License-Identifier: GPL
+ 
+  Copyright (C) 2020 The Yambo Team
+ 
+  Authors (see AUTHORS file for details): AM
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <wrapper.h>
+#include <kind.h>
+#include <driver.h>
+#include <string.h>
+
+char *running_libraries()
+{
+ int i_str,str_len,i_c;
+ char strings[20][20], *c;
+ i_str=0;
+#if defined _MPI
+ strcpy(strings[i_str], "MPI");
+#else
+ strcpy(strings[i_str], "Serial");
+#endif
+#if defined _OPENMP
+ i_str++;
+ strcpy(strings[i_str], "OpenMP");
+#endif
+#if defined _GPU
+ i_str++;
+ strcpy(strings[i_str], "GPU");
+#endif
+#if defined _CUDA
+ i_str++;
+ strcpy(strings[i_str], "CUDA");
+#endif
+#if defined _CUDAF
+ i_str++;
+ strcpy(strings[i_str], "CUDAF");
+#endif
+#if defined _OPENACC
+ i_str++;
+ strcpy(strings[i_str], "OpenACC");
+#endif
+#if defined _OPENMP_GPU
+ i_str++;
+ strcpy(strings[i_str], "OpenMP-GPU");
+#endif
+#if defined _SCALAPACK
+ i_str++;
+ strcpy(strings[i_str], "SLK");
+#endif
+#if defined _SLEPC
+ i_str++;
+ strcpy(strings[i_str], "SLEPC");
+#endif
+#if defined _PAR_IO
+ i_str++;
+ strcpy(strings[i_str], "HDF5_MPI_IO");
+#elif defined _HDF5_IO
+ i_str++;
+ strcpy(strings[i_str], "HDF5_IO");
+#elif defined _HDF5_LIB
+ i_str++;
+ strcpy(strings[i_str], "HDF5_LIB");
+#endif 
+ str_len=0;
+ for(i_c=0;i_c<=i_str;i_c++) {
+  str_len=str_len+sizeof(strings[i_c]);
+ }
+ c = malloc(str_len+1);
+ strcpy(c,"");
+ for(i_c=0;i_c<=i_str;i_c++) {
+  if (i_c>0) strcat(c,"+");
+  strcat(c,strings[i_c]);
+ }
+ return c;
+}
+void C_FUNC(get_libraries, GET_LIBRARIES)(char *libraries)
+{
+ char *c = running_libraries();
+ int len = strlen(c);
+ strcpy(libraries, c);
+ libraries[len] = libraries[len + 1];
+}
+

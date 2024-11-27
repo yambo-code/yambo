f7562267d1076377736719f8f10da92ceb2264f6
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2024-11-27 15:30:51 +0100

    Version 5.2.0,  Revision 23857,  Hash ee1983a80f
    MODIFIED *  configure include/headers/common/y_memory.h include/version/version.m4 allocations/DIPOLE_ALLOC_elemental.F io/io_X.F
    
    Bugs:
     -Many fixes for the compilation
    
    Additions:
    -
    
    Changes:
    -
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- include/headers/common/y_memory.h
+++ include/headers/common/y_memory.h
@@ -46,11 +46,72 @@
   allocate(x, mold=y,  &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
   YAMBO_ALLOC_CHECK(x)
 
-#define YAMBO_FREE(x) \
+ /* free */
+
+#define YAMBO_FREE_NO_DEV_CHECK(x) \
   if (.not.allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
   if (     allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
   if (     allocated(x)) &NEWLINE& deallocate(x)
+
+#if defined _OPENACC || defined _OPENMP_GPU
+#define YAMBO_FREE(x) \
+  if ( .not.allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
+  if (      allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
+  if ( devxlib_mapped(x)) &NEWLINE& call error(QUOTES Trying to deallocate var x still on device memory QUOTES)NEWLINE \
+  if (      allocated(x)) &NEWLINE& deallocate(x)
+#else
+#define YAMBO_FREE(x) YAMBO_FREE_NO_DEV_CHECK(x)
+#endif
+
 #define YAMBO_FREE_P(x) \
   if (.not.associated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
   if (     associated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
   if (     associated(x)) &NEWLINE& deallocate(x);nullify(x)
+
+ /* device allocations, OPENACC/OPENMP_GPU, CUDAF */
+
+#if defined _OPENACC || defined _OPENMP_GPU
+
+#define YAMBO_ALLOC_CHECK_GPU(x) \
+  if (     allocated(x)) &NEWLINE& call MEM_count_d(QUOTES x QUOTES,x)NEWLINE \
+  if (.not.allocated(x)) &NEWLINE& call MEM_error(QUOTES x QUOTES)
+#define YAMBO_ALLOC_GPU(x,SIZE) \
+  if (.not.allocated(x)) then NEWLINE YAMBO_ALLOC(x,SIZE) NEWLINE \
+  endif NEWLINE \
+  call devxlib_map(x) NEWLINE \
+  YAMBO_ALLOC_CHECK_GPU(x)
+#define YAMBO_ALLOC_GPU_SOURCE(x,y) \
+   if (.not.allocated(x)) then NEWLINE call error("[ALLOC] x not allocated") NEWLINE	\
+  endif NEWLINE \
+  call devxlib_map(x) NEWLINE \
+  call devxlib_memcpy_h2d(x,y) NEWLINE \
+  YAMBO_ALLOC_CHECK_GPU(x)
+#define YAMBO_ALLOC_GPU_MOLD(x,y) \
+  if (.not.allocated(x)) then NEWLINE YAMBO_ALLOC_MOLD(x,y) NEWLINE \
+  endif NEWLINE \
+  call devxlib_map(x) NEWLINE \
+  YAMBO_ALLOC_CHECK_GPU(x)
+
+#define YAMBO_FREE_GPU(x) \
+  if (.not.allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
+  if (     allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
+  if (     allocated(x)) &NEWLINE& call devxlib_unmap(x,MEM_err)
+
+#else
+
+#define YAMBO_ALLOC_GPU(x,SIZE) \
+  allocate(x SIZE,  &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
+  YAMBO_ALLOC_CHECK(x)
+#define YAMBO_ALLOC_GPU_SOURCE(x,y) \
+  allocate(x, source=y,  &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
+  YAMBO_ALLOC_CHECK(x)
+#define YAMBO_ALLOC_GPU_MOLD(x,y) \
+  allocate(x, mold=y,  &NEWLINE& stat=MEM_err,errmsg=MEM_msg)NEWLINE \
+  YAMBO_ALLOC_CHECK(x)
+
+#define YAMBO_FREE_GPU(x) \
+  if (.not.allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,int(-1,KIND=IPL))NEWLINE \
+  if (     allocated(x)) &NEWLINE& call MEM_free(QUOTES x QUOTES,size(x,KIND=IPL))NEWLINE \
+  if (     allocated(x)) &NEWLINE& deallocate(x)
+
+#endif

e5c0c10ceb5dc19a8a0b544dfbbf0cf5760defc0
Davide Sangalli <davide.sangalli@gmail.com>
2024-11-27 16:48:56 +0100

    Version 5.2.0,  Revision 23862,  Hash cb4195b08d
    MODIFIED *  config/mk/global/actions/compile_interfaces.mk config/mk/global/actions/compile_yambo.mk config/mk/global/actions/compile_ypp.mk include/version/version.m4
    
    Changes:
    - added definitions of yambo / ypp /interfaces
    
    Patch sent by:  Davide Sangalli <davide.sangalli@gmail.com>

--- config/mk/global/actions/compile_ypp.mk
+++ config/mk/global/actions/compile_ypp.mk
@@ -52,7 +52,7 @@ ypp ypp_ph ypp_sc ypp_rt_gpl ypp_rt ypp_nl:
 	@$(MAKE) $(MAKEFLAGS) dependencies
 	@$(MAKE) $(MAKEFLAGS) ext-libs
 	@$(MAKE) $(MAKEFLAGS) int-libs
-	@+LIBS="$(Y_SRC_LIBS)";LAB="_Y_";BASE="src";ADF="$(Y_PRECMP)";$(todo_lib);$(mk_lib)
+	@+LIBS="$(Y_SRC_LIBS)";LAB="_Y_";BASE="src";ADF="$(Y_PRECMP) -D_ypp";$(todo_lib);$(mk_lib)
 	@+LIBS="$(YPP_SRC_LIBS)";LAB="_YPP_";BASE="ypp";ADF="$(YPP_PRECMP)";$(todo_lib);$(mk_lib)
 	@+X2DO="$@";BASE="driver";XLIBS="$(Y_EXE_LIBS)";X_ypp_LIBS="$(YPP_EXE_LIBS)";ADF="$(YPP_PRECMP)";$(todo_driver)
 	@+X2DO="$@";BASE="driver";XLIBS="$(Y_EXE_LIBS)";X_ypp_LIBS="$(YPP_EXE_LIBS)";ADF="$(YPP_PRECMP)";$(mk_exe)

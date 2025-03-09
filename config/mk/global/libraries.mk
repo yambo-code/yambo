068962d33c8d83ad1add673d596f78d8dc92dc0e
Claudio Attaccalite <claudio.attaccalite@gmail.com>
2025-03-09 12:47:18 +0100

    Version 5.3.0,  Revision 24032,  Hash 6467e83ee
    MODIFIED *  config/mk/global/libraries.mk configure include/version/version.m4
    
    Bugs:
    -
    
    Additions:
     -Test lib conf
    
    Changes:
    -
    
    Patch sent by:  Claudio Attaccalite <claudio.attaccalite@gmail.com>

--- config/mk/global/libraries.mk
+++ config/mk/global/libraries.mk
@@ -13,16 +13,16 @@ endif
 include lib/archive/package.list
 #
 INT_LIBS      = qe_pseudo slatec math77 local
-YAMBO_INT_LIBS= Yio 
-YLIBIO        = modules Yio
+YAMBO_INT_LIBS= $(yio_dir)
+YLIBIO        = modules $(yio_dir)
 YLIBIO_LD     = $(YLIBIO)
 #
 # Yambo folders 
 #===============
 #
-BASIC_LIBS   = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing Yio io $(IO_MODE) \
+BASIC_LIBS   = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing $(yio_dir) io $(IO_MODE) \
                xc_functionals interface stop_and_restart wf_and_fft bz_ops coulomb
-BASIC_LIBS_LD= driver tools memory allocations communicate modules matrices linear_algebra bz_ops parallel parser output common timing Yio io $(IO_MODE) \
+BASIC_LIBS_LD= driver tools memory allocations communicate modules matrices linear_algebra bz_ops parallel parser output common timing $(yio_dir) io $(IO_MODE) \
                xc_functionals interface stop_and_restart wf_and_fft coulomb
 
 MAIN_LIBS    = $(BASIC_LIBS) interpolate qp_control setup tddft dipoles pol_function qp acfdt bse
@@ -55,9 +55,9 @@ PJ_NLLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup \
 # Yambo folders needed by Interfaces
 #=====================================
 #
-2Y_LIBS       = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing Yio io $(IO_MODE) \
+2Y_LIBS       = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing $(yio_dir) io $(IO_MODE) \
                 setup interface stop_and_restart bz_ops 
-2Y_LIBS_LD    = driver tools memory allocations communicate modules matrices linear_algebra parallel parser output common timing Yio io $(IO_MODE) \
+2Y_LIBS_LD    = driver tools memory allocations communicate modules matrices linear_algebra parallel parser output common timing $(yio_dir) io $(IO_MODE) \
                 setup interface stop_and_restart bz_ops 
 #
 # YPP folders 

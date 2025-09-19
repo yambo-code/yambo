0db203eec186d703321c11280c7add678cfe9fa3
thge123 <torstengeirs@gmail.com>
2025-09-19 10:32:45 +0200

    first commit

---
+++ symlink_modules.sh
@@ -0,0 +1,7 @@
+#/bin/zsh -l
+
+ln -s /home/tgeirsson/lumen-fork/bin/yambo ~/apps/lumen-fork/bin/yambo
+ln -s /home/tgeirsson/lumen-fork/bin/p2y   ~/apps/lumen-fork/bin/p2y
+ln -s /home/tgeirsson/lumen-fork/bin/a2y   ~/apps/lumen-fork/bin/a2y
+ln -s /home/tgeirsson/lumen-fork/bin/c2y   ~/apps/lumen-fork/bin/c2y
+ln -s /home/tgeirsson/lumen-fork/bin/ypp   ~/apps/lumen-fork/bin/ypp

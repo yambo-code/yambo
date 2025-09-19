a515ae95ec72f70f246888b7dc9f4d4b1fc29da2
thge123 <torstengeirs@gmail.com>
2025-09-19 10:36:57 +0200

    .gitignore fix

--- symlink_modules.sh
+++
@@ -1,7 +0,0 @@
-#/bin/zsh -l
-
-ln -s /home/tgeirsson/lumen-fork/bin/yambo ~/apps/lumen-fork/bin/yambo
-ln -s /home/tgeirsson/lumen-fork/bin/p2y   ~/apps/lumen-fork/bin/p2y
-ln -s /home/tgeirsson/lumen-fork/bin/a2y   ~/apps/lumen-fork/bin/a2y
-ln -s /home/tgeirsson/lumen-fork/bin/c2y   ~/apps/lumen-fork/bin/c2y
-ln -s /home/tgeirsson/lumen-fork/bin/ypp   ~/apps/lumen-fork/bin/ypp

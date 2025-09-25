90e3b2e83ea5c028614f11228f29f96762ad9379
Davide Sangalli <davide.sangalli@gmail.com>
2025-09-26 00:10:48 +0200

    title contains both old yambo and new lumen options

--- src/driver/title.c
+++ src/driver/title.c
@@ -10,7 +10,35 @@
 #include <stdio.h>
 #include <string.h>
 
-void title(FILE *file_name, char *cmnt, struct tool_struct t)
+void title_yambo(FILE *file_name, char *cmnt, struct tool_struct t)
+{
+    char *tool = running_tool();
+    char *pj = running_project();
+    char *libs = running_libraries();
+    fprintf(file_name, "%s%s\n", cmnt, " ___ __  _____  __ __  _____   _____ ");
+    fprintf(file_name, "%s%s\n", cmnt, "|   Y  ||  _  ||  Y  ||  _  \\ |  _  |");
+    fprintf(file_name, "%s%s\n", cmnt, "|   |  ||. |  ||.    ||. |  / |. |  |");
+    fprintf(file_name, "%s%s\n", cmnt, " \\   _/ |. _  ||.\\ / ||. _  \\ |. |  |");
+    fprintf(file_name, "%s%s\n", cmnt, "  |: |  |: |  ||: |  ||: |   \\|: |  |");
+    fprintf(file_name, "%s%s\n", cmnt, "  |::|  |:.|:.||:.|:.||::.   /|::.  |");
+    fprintf(file_name, "%s%s\n", cmnt, "  `--\"  `-- --\"`-- --\"`-----\" `-----\"");
+    fprintf(file_name, "%s '%s' \n", cmnt, t.desc);
+    if (strlen(pj) > 0)
+    {
+        fprintf(file_name, "%s\n%s This is      : %s(%s)", cmnt, cmnt, tool,
+                pj);
+    }
+    else
+    {
+        fprintf(file_name, "%s\n%s This is      : %s", cmnt, cmnt, tool);
+    }
+    fprintf(file_name, "%s\n%s Version      : %s ", cmnt, cmnt,
+            t.version_string);
+    fprintf(file_name, "%s\n%s Configuration: %s \n", cmnt, cmnt, libs);
+};
+
+
+void title_lumen(FILE *file_name, char *cmnt, struct tool_struct t)
 {
     char *tool = running_tool();
     char *pj = running_project();

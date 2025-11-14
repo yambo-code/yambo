9f037eb92c93af32ea492682c72e6b50806fc51e
Murali <muralidharrsvm7@gmail.com>
2025-11-14 22:46:53 +0100

    cleanup macros in driver headers

--- include/driver/fortran_driver.h
+++ include/driver/fortran_driver.h
@@ -11,24 +11,19 @@
 /* YAMBO
  */
 #pragma once
+#include "wrapper.h"
 
-#if defined _FORTRAN_US
-#define FORTRAN_DRIVER_MACRO(X)                                        \
-    int X##_(int *, int *, int *, int *, int *, int *, int *, int *,   \
-             char *string, char *in_file, char *in_dir, char *out_dir, \
-             char *com_dir, char *job, int string_N, int in_file_N,    \
-             int in_dir_N, int out_dir_N, int com_dir_N, int job_N);
-#else
-#define FORTRAN_DRIVER_MACRO(X)                                                \
-    int X(int *, int *, int *, int *, int *, int *, int *, int *,              \
-          char *string, char *in_file, char *in_dir, char *out_dir,            \
-          char *com_dir, char *job, int string_N, int in_file_N, int in_dir_N, \
-          int out_dir_N, int com_dir_N, int job_N);
-#endif
+// Common prototype for all FORTRAN driver entry points
+typedef int FortranDriverFn(int *, int *, int *, int *, int *, int *, int *,
+                            int *, char *string, char *in_file, char *in_dir,
+                            char *out_dir, char *com_dir, char *job,
+                            int string_N, int in_file_N, int in_dir_N,
+                            int out_dir_N, int com_dir_N, int job_N);
 
-FORTRAN_DRIVER_MACRO(yambo);
-FORTRAN_DRIVER_MACRO(ypp);
-FORTRAN_DRIVER_MACRO(a2y);
-FORTRAN_DRIVER_MACRO(p2y);
-FORTRAN_DRIVER_MACRO(c2y);
-FORTRAN_DRIVER_MACRO(e2y);
+// Declare driver functions
+extern FortranDriverFn F90_FUNC(yambo);
+extern FortranDriverFn F90_FUNC(ypp);
+extern FortranDriverFn F90_FUNC(a2y);
+extern FortranDriverFn F90_FUNC(p2y);
+extern FortranDriverFn F90_FUNC(c2y);
+extern FortranDriverFn F90_FUNC(e2y);
